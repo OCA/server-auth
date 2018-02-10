d952a47880541f3702e3582742760b465e177913
Thierry Ducrest <thierry@ducrest.net>
2018-02-10 03:34:01 -0300

    Remove unnecessary overriding of check method
    The parent method calls check_credentials which is also implemented here,
    so this is not needed.

--- auth_from_http_remote_user/models/res_users.py
+++ auth_from_http_remote_user/models/res_users.py
@@ -2,9 +2,8 @@
 # Copyright 2014-2018 'ACSONE SA/NV'
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-import odoo
 from odoo import fields, models
-from odoo.addons.auth_from_http_remote_user import utils
+from .. import utils
 
 
 class Users(models.Model):
@@ -17,26 +16,19 @@ class Users(models.Model):
         copy=False
     )
 
+    @classmethod
+    def authenticate_sso_user(cls, env, user):
+        """Specific authentication for HTTP user.
+
+        Generate a key for authentication and update the user
+        """
+        key = utils.randomString(utils.KEY_LENGTH, '0123456789abcdef')
+        user.with_env(env).sudo().write({'sso_key': key})
+        return key
+
     def check_credentials(self, password):
+        """Check credentials for SSO user"""
         res = self.sudo().search([('id', '=', self._uid),
                                   ('sso_key', '=', password)])
         if not res:
             return super(Users, self).check_credentials(password)
-
-    @classmethod
-    def check(cls, db, uid, passwd):
-        try:
-            return super(Users, cls).check(db, uid, passwd)
-        except odoo.exceptions.AccessDenied:
-            if not passwd:
-                raise
-            registry = odoo.registry(db)
-            with registry.cursor() as cr:
-                cr.execute('''SELECT COUNT(1)
-                                FROM res_users
-                               WHERE id=%s
-                                 AND sso_key=%s
-                                 AND active=%s''', (int(uid), passwd, True))
-                if not cr.fetchone()[0]:
-                    raise
-                cls._uid_cache.setdefault(db, {})[uid] = passwd
