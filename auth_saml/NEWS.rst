0367338774638a54daa6ebe836675bee3efe2c9a
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2018-06-08 11:40:53 +0200

    remove group no one from saml provider

---
+++ auth_saml/NEWS.rst
@@ -0,0 +1,7 @@
+Changelog
+=========
+
+11.0.1.0.1
+----------
+
+Viewing SAML providers does not need to be in debug mode anymore.
--- auth_saml/__manifest__.py
+++ auth_saml/__manifest__.py
@@ -3,7 +3,7 @@
 
 {
     'name': 'Saml2 Authentication',
-    'version': '11.0.1.0.0',
+    'version': '11.0.1.0.1',
     'category': 'Tools',
     'author': 'XCG Consulting, Odoo Community Association (OCA)',
     'maintainer': 'XCG Consulting',
