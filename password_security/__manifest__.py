703d023d593f513e21ea7e35cb8cad8adad51548
Jairo Llopis <Yajo@users.noreply.github.com>
2018-06-22 11:43:30 +0100

    [FIX] password_security: Allow mini-admin to create users (#31)
    In a normal Odoo deployment, somebody in group *Administration / Access Rights* should be able to create users; but if this addon is installed, it gets this error:
    
        The requested operation cannot be completed due to security restrictions. Please contact your system administrator.
    
        (Document type: Res Users Password History, Operation: create)
    
    This is now tested and fixed.

--- password_security/__manifest__.py
+++ password_security/__manifest__.py
@@ -4,7 +4,7 @@
 
     'name': 'Password Security',
     "summary": "Allow admin to set password security requirements.",
-    'version': '11.0.1.0.1',
+    'version': '11.0.1.1.0',
     'author':
         "LasLabs, "
         "Kaushal Prajapati, "
