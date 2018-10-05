0e91d2304ca40dd6fe520246a6dec71ae540a8c1
André Schenkels <a.schenkels@ictstudio.eu>
2018-10-05 15:43:47 +0200

    [ADD] auth_oidc module

---
+++ auth_oidc/models/__init__.py
@@ -0,0 +1,6 @@
+# -*- coding: utf-8 -*-
+# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
+
+from . import auth_oauth_provider
+from . import res_users
