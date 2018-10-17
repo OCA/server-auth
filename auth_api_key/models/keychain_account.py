08c193efaf0e58f307f75dc0369dc1021ade146c
hparfr <raph@hpar.fr>
2018-10-17 12:51:51 +0200

    Since keychain 10.0.2.0.1 there is a default validation method

--- auth_api_key/models/keychain_account.py
+++ auth_api_key/models/keychain_account.py
@@ -2,7 +2,7 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class KeychainAccount(models.Model):
@@ -11,11 +11,3 @@ class KeychainAccount(models.Model):
 
     namespace = fields.Selection(
         selection_add=[('auth_api_key', 'Authentification Api key')])
-
-    @api.multi
-    def _auth_api_key_validate_data(self, data):
-        return True
-
-    @api.multi
-    def _auth_api_key_init_data(self):
-        return {}
