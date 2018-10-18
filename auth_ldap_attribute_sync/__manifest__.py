a8f97fc18efd0e9ec492a9906794aa6183341a80
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-10-18 07:45:57 +0300

    [ADD] auth_ldap_attribute_sync

---
+++ auth_ldap_attribute_sync/__manifest__.py
@@ -0,0 +1,29 @@
+# Copyright (C) 2018 Brainbean Apps (https://brainbeanapps.com)
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+
+{
+    'name': 'LDAP Attribute Sync',
+    'version': '12.0.1.0.0',
+    'category': 'Tools',
+    'website': 'https://github.com/OCA/server-auth',
+    'author':
+        'Braibean Apps, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'installable': True,
+    'application': False,
+    'summary': 'Allows to update users’ fields from LDAP attributes',
+    'depends': [
+        'auth_ldap',
+    ],
+    'external_dependencies': {
+        'python': [
+            'ldap',
+        ],
+    },
+    'data': [
+        'security/ir.model.access.csv',
+        'views/res_company_ldap_views.xml',
+        'views/ldap_attribute_mapping_views.xml',
+    ],
+}
---
+++ auth_ldap_attribute_sync/readme/CONFIGURE.rst
@@ -0,0 +1,4 @@
+To configure this module, you need to:
+
+#. Access Settings / General Settings / LDAP Authentication / LDAP Server
+#. Configure attributes mapping according to the LDAP server configuration
