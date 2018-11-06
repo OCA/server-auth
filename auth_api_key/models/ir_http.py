09740717757e6ab580d09ed36f95ebb133086e55
Quentin Groulard <quentin.groulard@acsone.eu>
2018-11-06 12:34:52 +0100

    Keychain removed
    Get rid of keychain dependency

--- auth_api_key/models/ir_http.py
+++ auth_api_key/models/ir_http.py
@@ -15,24 +15,22 @@ _logger = logging.getLogger(__name__)
 
 
 class IrHttp(models.AbstractModel):
-    _inherit = 'ir.http'
+    _inherit = "ir.http"
 
     @classmethod
     def _auth_method_api_key(cls):
         headers = request.httprequest.environ
-        api_key = headers.get('HTTP_API_KEY')
+        api_key = headers.get("HTTP_API_KEY")
         if api_key:
             request.uid = 1
-            auth_api_key = request.env['auth.api.key'].retrieve_from_api_key(
-                api_key
-            )
-            if auth_api_key:
+            uid = request.env["auth.api.key"].retrieve_uid_from_api_key(api_key)
+            if uid:
                 # reset _env on the request since we change the uid...
                 # the next call to env will instantiate an new
                 # odoo.api.Environment with the user defined on the
                 # auth.api_key
                 request._env = None
-                request.uid = auth_api_key.user_id.id
+                request.uid = uid
                 request.auth_api_key = auth_api_key
                 return True
         _logger.error("Wrong HTTP_API_KEY, access denied")
