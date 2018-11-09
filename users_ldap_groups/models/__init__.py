e9d9d8af343884488d3858e24aef50eef3bac843
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-09 07:25:53 +0200

    [MIG] users_ldap_groups: Migration to 12.0

--- users_ldap_groups/models/__init__.py
+++ users_ldap_groups/models/__init__.py
@@ -1,5 +1,3 @@
-# -*- coding: utf-8 -*-
-# Copyright 2012-2018 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from . import res_company_ldap
 from . import res_company_ldap_operator
