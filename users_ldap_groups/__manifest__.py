ad15e3aa96d7d6f04aba840cd1499ad2c3af78ea
Alexey Pelykh <alexey.pelykh@gmail.com>
2018-11-09 07:25:53 +0200

    [MIG] users_ldap_groups: Migration to 12.0

--- users_ldap_groups/__manifest__.py
+++ users_ldap_groups/__manifest__.py
@@ -1,20 +1,27 @@
-# -*- coding: utf-8 -*-
 # Copyright 2012-2018 Therp BV <https://therp.nl>
+# Copyright 2018 Brainbean Apps <https://brainbeanapps.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
-    "name": "LDAP groups assignment",
-    "version": "10.0.0.0.0",
-    "depends": ["auth_ldap"],
-    "author": "Therp BV, Odoo Community Association (OCA)",
-    "license": "AGPL-3",
-    "summary": "Adds user accounts to groups based on rules defined "
-    "by the administrator.",
-    "category": "Authentication",
-    "data": [
-        'views/base_config_settings.xml',
+    'name': 'LDAP groups assignment',
+    'version': '12.0.1.0.0',
+    'depends': [
+        'auth_ldap',
+    ],
+    'author':
+        'Therp BV, '
+        'Odoo Community Association (OCA)',
+    'license': 'AGPL-3',
+    'summary':
+        'Adds user accounts to groups based on rules defined '
+        'by the administrator.',
+    'category': 'Authentication',
+    'data': [
+        'views/res_company_ldap_views.xml',
         'security/ir.model.access.csv',
     ],
-    "external_dependencies": {
-        'python': ['ldap'],
+    'external_dependencies': {
+        'python': [
+            'ldap'
+        ],
     },
 }
--- users_ldap_groups/models/res_company_ldap_operator.py
+++ users_ldap_groups/models/res_company_ldap_operator.py
@@ -1,9 +1,10 @@
-# -*- coding: utf-8 -*-
-# © 2012-2018 Therp BV <https://therp.nl>
+# Copyright 2012-2018 Therp BV <https://therp.nl>
+# Copyright 2018 Brainbean Apps <https://brainbeanapps.com>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 from logging import getLogger
 from odoo import api, models
 from string import Template
+
 _logger = getLogger(__name__)
 
 
@@ -18,26 +19,33 @@ class ResCompanyLdapOperator(models.AbstractModel):
         """Return names of function to call on this model as operator"""
         return ('contains', 'equals', 'query')
 
-    @api.model
     def contains(self, ldap_entry, mapping):
         return mapping.ldap_attribute in ldap_entry[1] and \
-            mapping.value in ldap_entry[1][mapping.ldap_attribute]
-
-    def equals(self, ldap_entry, mapping):
-        return mapping.ldap_attribute in ldap_entry[1] and \
-            unicode(mapping.value) == unicode(
+            mapping.value in map(
+                lambda x: x.decode(),
                 ldap_entry[1][mapping.ldap_attribute]
             )
 
+    def equals(self, ldap_entry, mapping):
+        return mapping.ldap_attribute in ldap_entry[1] and \
+            mapping.value == str(list(map(
+                lambda x: x.decode(),
+                ldap_entry[1][mapping.ldap_attribute]
+            )))
+
     def query(self, ldap_entry, mapping):
         query_string = Template(mapping.value).safe_substitute({
-            attr: ldap_entry[1][attr][0] for attr in ldap_entry[1]
+            attr: ldap_entry[1][attr][0].decode() for attr in ldap_entry[1]
         })
+
+        results = mapping.ldap_id._query(
+            mapping.ldap_id.read()[0],
+            query_string
+        )
         _logger.debug(
-            'evaluating query group mapping, filter: %s' % query_string
+            'Performed LDAP query "%s" results: %s',
+            query_string,
+            results
         )
-        results = mapping.ldap_id.query(
-            mapping.ldap_id.read()[0], query_string
-        )
-        _logger.debug(results)
+
         return bool(results)
