c59dc5110e84041e8febfa0dc49048d0e0609ffd
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2018-11-13 11:32:39 +0100

    [FIX] auth_api_key: fix api and flake8

--- auth_api_key/models/auth_api_key.py
+++ auth_api_key/models/auth_api_key.py
@@ -2,7 +2,7 @@
 # Copyright 2018 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models, tools
+from odoo import api, models, tools, _
 
 from odoo.addons.server_environment import serv_config
 
@@ -16,20 +16,22 @@ class AuthApiKey(models.TransientModel):
     @tools.ormcache("api_key")
     def _retrieve_uid_from_api_key(self, api_key):
         if not self.env.user.has_group("base.group_system"):
-            raise AccessError("User is not allowed")
+            raise AccessError(_("User is not allowed"))
 
         for section in serv_config.sections():
             if section.startswith("api_key_") and serv_config.has_option(
-                section, "key"
+                    section, "key"
             ):
                 if api_key != serv_config.get(section, "key"):
                     continue
 
                 login_name = serv_config.get(section, "user")
-                uid = self.env["res.users"].search([("login", "=", login_name)]).id
+                uid = self.env["res.users"].search(
+                    [("login", "=", login_name)]).id
 
                 if not uid:
-                    raise ValidationError("No user found with login %s" % login_name)
+                    raise ValidationError(
+                        _("No user found with login %s") % login_name)
 
                 return uid
         return False
