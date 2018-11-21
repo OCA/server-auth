a1ce7b9d940817f12457e109577131534a7c9886
Stéphane Bidoul (ACSONE) <stephane.bidoul@acsone.eu>
2018-11-21 11:07:12 +0100

    [CHG] auth_api_key is LGPL

--- auth_api_key/__manifest__.py
+++ auth_api_key/__manifest__.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 # Copyright 2018 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Auth Api Key",
     "summary": """
         Authenticate http requests from an API key""",
     "version": "12.0.1.0.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://acsone.eu/",
     "development_status": "Beta",
