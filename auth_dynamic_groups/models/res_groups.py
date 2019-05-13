905f6a6e37f2542bcdb37006d3c0403eeba8be3b
Holger Brunn <hbrunn@therp.nl>
2019-05-13 15:21:51 +0200

    [MIG] auth_dynamic_groups to v10

--- auth_dynamic_groups/models/res_groups.py
+++ auth_dynamic_groups/models/res_groups.py
@@ -1,26 +1,8 @@
 # -*- coding: utf-8 -*-
-##############################################################################
-#
-#    OpenERP, Open Source Management Solution
-#    This module copyright (C) 2013 Therp BV (<http://therp.nl>).
-#
-#    This program is free software: you can redistribute it and/or modify
-#    it under the terms of the GNU Affero General Public License as
-#    published by the Free Software Foundation, either version 3 of the
-#    License, or (at your option) any later version.
-#
-#    This program is distributed in the hope that it will be useful,
-#    but WITHOUT ANY WARRANTY; without even the implied warranty of
-#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-#    GNU Affero General Public License for more details.
-#
-#    You should have received a copy of the GNU Affero General Public License
-#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
-##############################################################################
-from openerp import models, fields, api, exceptions
-from openerp.tools.safe_eval import safe_eval
-from openerp import _
+# Copyright 2013-2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import _, api, exceptions, fields, models
+from odoo.tools.safe_eval import safe_eval
 
 
 class ResGroups(models.Model):
@@ -60,4 +42,4 @@ class ResGroups(models.Model):
     def action_evaluate(self):
         res_users = self.env['res.users']
         for user in res_users.search([]):
-            res_users._model.update_dynamic_groups(user.id, self.env.cr.dbname)
+            res_users.update_dynamic_groups(user.id, self.env.cr.dbname)
---
+++ auth_dynamic_groups/models/res_users.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo import models, SUPERUSER_ID
+from odoo.api import Environment
+
+
+class ResUsers(models.Model):
+    _inherit = 'res.users'
+
+    @classmethod
+    def _login(self, db, login, password):
+        uid = super(ResUsers, self)._login(db, login, password)
+
+        if uid and uid != SUPERUSER_ID:
+            self.update_dynamic_groups(uid, db)
+
+        return uid
+
+    @classmethod
+    def update_dynamic_groups(self, uid, db):
+        with self.pool.cursor() as cr:
+            env = Environment(cr, SUPERUSER_ID, {})
+            dynamic_groups = env['res.groups'].search([
+                ('is_dynamic', '=', True)
+            ])
+            if dynamic_groups:
+                cr.execute(
+                    'delete from res_groups_users_rel '
+                    'where uid=%s and gid in %s',
+                    (uid, tuple(dynamic_groups.ids))
+                )
+            for dynamic_group in dynamic_groups:
+                if dynamic_group.eval_dynamic_group_condition(uid=uid):
+                    cr.execute(
+                        'insert into res_groups_users_rel (uid, gid) values '
+                        '(%s, %s)',
+                        (uid, dynamic_group.id),
+                    )
+            env['res.users'].invalidate_cache(['groups_id'], [uid])
