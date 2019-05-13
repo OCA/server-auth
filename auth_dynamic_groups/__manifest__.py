350e3cab67184b2d786682b61ad24669455ee01e
Holger Brunn <hbrunn@therp.nl>
2019-05-13 15:21:51 +0200

    [MIG] auth_dynamic_groups to v10

---
+++ auth_dynamic_groups/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# Copyright 2013-2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Dynamic groups",
+    "version": "10.0.1.0.0",
+    "author": "Odoo Community Association (OCA),Therp BV",
+    "license": "AGPL-3",
+    "complexity": "normal",
+    'summary': 'Have membership conditions for certain groups',
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-auth",
+    "depends": [
+        'base',
+    ],
+    "data": [
+        'views/res_groups.xml',
+    ],
+}
