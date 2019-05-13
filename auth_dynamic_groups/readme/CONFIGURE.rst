4a664632e6f6a25ac6355e64299db79c2943bfb6
Holger Brunn <hbrunn@therp.nl>
2019-05-13 15:21:51 +0200

    [MIG] auth_dynamic_groups to v10

---
+++ auth_dynamic_groups/readme/CONFIGURE.rst
@@ -0,0 +1,7 @@
+Check `Dynamic` on a group you want to be dynamic. Now fill in the condition,
+using `user` which is a browse record of the user in question that evaluates
+truthy if the user is supposed to be a member of the group and falsy if not.
+
+There is a constraint on the field to check for validity if this expression.
+When you're satisfied, click the button `Evaluate` to prefill the group's
+members. The condition will be checked now for every user who logs in.
---
+++ auth_dynamic_groups/tests/test_auth_dynamic_groups.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+# Copyright 2019 Therp BV <https://therp.nl>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
+from odoo.tests.common import TransactionCase
+
+
+class TestAuthDynamicGroups(TransactionCase):
+    def test_auth_dynamic_groups(self):
+        # without this, the below happens in another transaction we don't see
+        self.env.registry.enter_test_mode()
+        env = self.env(cr=self.env.registry.cursor())
+        demo_user = env.ref('base.user_demo')
+        group = env['res.groups'].create({
+            'name': 'dynamic group',
+            'is_dynamic': True,
+            'dynamic_group_condition': "'Demo' in user.name",
+        })
+        group.action_evaluate()
+        group.refresh()
+        self.assertIn(demo_user, group.users)
+        group.write({'users': [(6, False, [])]})
+        self.assertFalse(group.users)
+        self.env['res.users']._login(self.env.cr.dbname, 'demo', 'demo')
+        group.refresh()
+        self.assertIn(demo_user, group.users)
+        self.env.registry.leave_test_mode()
