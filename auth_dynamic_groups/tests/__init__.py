cc2e67aa5a2801e5280634c627cf8f7ba80e039d
Holger Brunn <hbrunn@therp.nl>
2019-05-13 15:21:51 +0200

    [MIG] auth_dynamic_groups to v10

--- auth_dynamic_groups/tests/__init__.py
+++ auth_dynamic_groups/tests/__init__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
-# Copyright 2013-2019 Therp BV <https://therp.nl>
+# Copyright 2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import models
+from . import test_auth_dynamic_groups
