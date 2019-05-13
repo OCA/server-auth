cc1633a82c48aeeb86439d0f9d69401be74dc0a0
Holger Brunn <hbrunn@therp.nl>
2019-05-13 15:21:51 +0200

    [MIG] auth_dynamic_groups to v10

--- auth_dynamic_groups/models/__init__.py
+++ auth_dynamic_groups/models/__init__.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
 # Copyright 2013-2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
-from . import models
+from . import res_users
+from . import res_groups
