f21bde90ddea500cc3936168f2d7b5a1604e7673
Ronald Portier <ronald@therp.nl>
2019-05-16 15:48:19 +0200

    [MIG] auth_dynamic_groups to v11.

--- auth_dynamic_groups/__manifest__.py
+++ auth_dynamic_groups/__manifest__.py
@@ -1,11 +1,11 @@
-# -*- coding: utf-8 -*-
 # Copyright 2013-2019 Therp BV <https://therp.nl>
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 {
     "name": "Dynamic groups",
-    "version": "10.0.1.0.0",
+    "version": "11.0.1.0.0",
     "author": "Odoo Community Association (OCA),Therp BV",
     "license": "AGPL-3",
+    "website": "https://github.com/OCA/server-auth",
     "complexity": "normal",
     'summary': 'Have membership conditions for certain groups',
     "category": "Tools",
@@ -17,7 +17,4 @@
     ],
     "auto_install": False,
     "installable": True,
-    "external_dependencies": {
-        'python': [],
-    },
 }
