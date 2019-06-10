63b47d5187902254431aded5c6891975d6cd875d
Luis <luis.garcia@savoirfairelinux.com>
2019-06-10 13:57:25 -0400

    [FIX] Create user only when it doesn't exist
    [FIX] Flake8 & pylint
    
    [FIX] manifest
    
    [FIX] manifest
    
    [UPD] auth_saml_create_user/models/auth_saml.py
    
    Co-Authored-By: Rim BEN DHAOU <bendhaou.rim@gmail.com>
    
    Apply suggestions from code review
    
    [ADD] Tests
    
    [ADD] Tests
    
    [ADD] Starting random password
    
    [FIX Imports and pep8]
    
    [UPD] Tests
    
    [FIX] empty password
    
    [FIX] exception not needed
    
    [FIX] password not needed in write
    
    [UPD] Create saml pass string
    
    [ADD] test secret problem
    
    [FIX] secret null
    
    [ADD]tests: teradown method
    
    [FIX] not need teardown
    
    [FIX] manifest
    
    [FIX] License
    
    [FIX] Maintainers and development_status
    
    Update __manifest__.py

--- auth_saml_create_user/models/__init__.py
+++ auth_saml_create_user/models/__init__.py
@@ -1,5 +1,5 @@
 # © 2018 Savoir-faire Linux
-# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 from . import auth_saml
 from . import res_users
