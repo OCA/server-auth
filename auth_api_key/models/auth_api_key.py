2858a7d25bd5b8d3fd80b624c5329d9b34e9a3ef
Mourad <mourad.elhadj.mimoune@akretion.com>
2019-07-18 13:47:24 +0200

    [IMP] ADD possibility to save api keys in database by using server.env.mixin obj

--- auth_api_key/models/auth_api_key.py
+++ auth_api_key/models/auth_api_key.py
@@ -3,36 +3,64 @@
 
 from odoo import api, models, tools, _
 
-from odoo.addons.server_environment import serv_config
 from odoo.tools import consteq
 
 from odoo.exceptions import ValidationError, AccessError
 
 
-class AuthApiKey(models.TransientModel):
+class AuthApiKey(models.Model):
     _name = "auth.api.key"
+    _inherit = "server.env.mixin"
     _description = "API Key Retriever"
+
+    name = fields.Char(required=True)
+    key = fields.Char(required=True)
+    user = fields.Char(required=True)
+
+    _sql_constraints = [
+        ('key_uniq', 'unique(key)', 'API Key Retriever must be unique !'),
+    ]
+
+    @property
+    def _server_env_fields(self):
+        base_fields = super()._server_env_fields
+        api_key_fields = {
+            "key": {},
+            "user": {},
+        }
+        api_key_fields.update(base_fields)
+        return api_key_fields
+
+    @api.model
+    @tools.ormcache("api_key")
+    def _retrieve_api_key(self, api_key):
+        if not self.env.user.has_group("base.group_system"):
+            raise AccessError(_("User is not allowed"))
+        ap_keys = self.search([])
+        # api key are a computed field in the context of server env
+        # so we can't use a domain in search method
+        key = False
+        for ap_key in ap_keys:
+            if consteq(api_key, ap_key.key):
+                key = ap_key
+
+        if not key:
+            raise ValidationError(
+                _("The key %s is not defined") % api_key)
+
+        return key
 
     @api.model
     @tools.ormcache("api_key")
     def _retrieve_uid_from_api_key(self, api_key):
-        if not self.env.user.has_group("base.group_system"):
-            raise AccessError(_("User is not allowed"))
+        ap_key = self._retrieve_api_key(api_key)
+        uid = self.env["res.users"].search(
+                    [("login", "=", ap_key.user)]).id
 
-        for section in serv_config.sections():
-            if section.startswith("api_key_") and serv_config.has_option(
-                    section, "key"
-            ):
-                if not consteq(api_key, serv_config.get(section, "key")):
-                    continue
+        if not uid:
+            raise ValidationError(
+                _("No user found with login %s") % login_name)
 
-                login_name = serv_config.get(section, "user")
-                uid = self.env["res.users"].search(
-                    [("login", "=", login_name)]).id
+        return uid
+    return False
 
-                if not uid:
-                    raise ValidationError(
-                        _("No user found with login %s") % login_name)
-
-                return uid
-        return False
