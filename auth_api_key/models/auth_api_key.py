687c946c8027cfafd250ee48a80a9ccbdd95ea8a
Sébastien BEAU <sebastien.beau@akretion.com>
2019-07-19 17:13:54 +0200

    [IMP] make auth_api_key compatible in multi-db
    Api key is now based on the new version of server env
    Key belong to a specifiv database that can be requested based on the
    domain name.
    
    Just setup the db_filter with "%d^" to filter based on domain name

--- auth_api_key/models/auth_api_key.py
+++ auth_api_key/models/auth_api_key.py
@@ -1,7 +1,7 @@
 # Copyright 2018 ACSONE SA/NV
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
-from odoo import api, models, tools, _
+from odoo import api, fields, models, tools, _
 
 from odoo.tools import consteq
 
@@ -15,52 +15,52 @@ class AuthApiKey(models.Model):
 
     name = fields.Char(required=True)
     key = fields.Char(required=True)
-    user = fields.Char(required=True)
-
-    _sql_constraints = [
-        ('key_uniq', 'unique(key)', 'API Key Retriever must be unique !'),
-    ]
+    user_id = fields.Many2one(
+        comodel_name="res.users",
+        string="User",
+        required=True)
 
     @property
     def _server_env_fields(self):
         base_fields = super()._server_env_fields
         api_key_fields = {
             "key": {},
-            "user": {},
         }
         api_key_fields.update(base_fields)
         return api_key_fields
 
     @api.model
-    @tools.ormcache("api_key")
-    def _retrieve_api_key(self, api_key):
-        if not self.env.user.has_group("base.group_system"):
-            raise AccessError(_("User is not allowed"))
-        ap_keys = self.search([])
-        # api key are a computed field in the context of server env
-        # so we can't use a domain in search method
-        key = False
-        for ap_key in ap_keys:
-            if consteq(api_key, ap_key.key):
-                key = ap_key
-
-        if not key:
-            raise ValidationError(
-                _("The key %s is not defined") % api_key)
-
-        return key
+    def _retrieve_api_key(self, key):
+        return self.browse(self._retrieve_api_key_id(key))
 
     @api.model
-    @tools.ormcache("api_key")
-    def _retrieve_uid_from_api_key(self, api_key):
-        ap_key = self._retrieve_api_key(api_key)
-        uid = self.env["res.users"].search(
-                    [("login", "=", ap_key.user)]).id
+    @tools.ormcache("key")
+    def _retrieve_api_key_id(self, key):
+        if not self.env.user.has_group("base.group_system"):
+            raise AccessError(_("User is not allowed"))
+        for api_key in self.search([]):
+            if consteq(key, api_key.key):
+                return api_key.id
+        raise ValidationError(_("The key %s is not allowed") % key)
 
-        if not uid:
-            raise ValidationError(
-                _("No user found with login %s") % ap_key.user)
+    @api.model
+    @tools.ormcache("key")
+    def _retrieve_uid_from_api_key(self, key):
+        return self._retrieve_api_key(key).user_id.id
 
-        return uid
-    return False
+    def _clear_key_cache(self):
+        self._retrieve_api_key_id.clear_cache()
+        self._retrieve_uid_from_api_key.clear_cache()
 
+    @api.model
+    def create(self, vals):
+        record = super(AuthApikey, self).create(vals)
+        if 'key' in vals:
+            self._clear_key_cache()
+        return record
+
+    def write(self, vals):
+        super(AuthApikey, self).write(vals)
+        if 'key' in vals:
+            self._clear_key_cache()
+        return True
