32209c4052157e229ed7e99fc07e61c513b48391
Sylvain LE GAL <sylvain.legal@grap.coop>
2019-08-09 11:43:38 +0200

    [PORT] 12.0: auth_admin_passkey [REF+IMP] move configuration for ir.config.parameter to odoo.cfg files, ref : https://github.com/OCA/server-auth/issues/21 So : - It's more GDPR compatible, because only system administrators that have already a full DB access can enable and use this feature - It's easier to have different settings, according to server env [REF] OCA convention (readme files, etc...)

--- auth_admin_passkey/models/__init__.py
+++ auth_admin_passkey/models/__init__.py
@@ -1,9 +1,1 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
-# @author Sylvain LE GAL (https://twitter.com/legalsylvain)
-# License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
-
-from . import (
-    res_config,
-    res_users,
-    )
+from . import res_users
--- auth_admin_passkey/models/res_users.py
+++ auth_admin_passkey/models/res_users.py
@@ -1,98 +1,73 @@
-# -*- coding: utf-8 -*-
-# Copyright (C) 2013-2014 GRAP (http://www.grap.coop)
+# Copyright (C) 2013-Today GRAP (http://www.grap.coop)
 # @author Sylvain LE GAL (https://twitter.com/legalsylvain)
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
-import datetime
+from datetime import datetime
+import logging
 
-from odoo import SUPERUSER_ID, _, api, exceptions, models
-from odoo.tools.safe_eval import safe_eval
+from odoo import _, api, exceptions, models, SUPERUSER_ID
+from odoo.tools import config
+
+logger = logging.getLogger(__name__)
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
 
     @api.model
-    def _send_email_passkey(self, user_id):
-        """ Send a email to the admin of the system and / or the user
+    def _send_email_passkey(self, login_user):
+        """ Send a email to the system administrator and / or the user
             to inform passkey use."""
-        mail_obj = self.env['mail.mail'].sudo()
-        icp_obj = self.env['ir.config_parameter']
+        MailMail = self.env['mail.mail'].sudo()
 
         admin_user = self.sudo().browse(SUPERUSER_ID)
-        login_user = self.browse(user_id)
 
-        send_to_admin = safe_eval(
-            icp_obj.get_param('auth_admin_passkey.send_to_admin')
-        )
-        send_to_user = safe_eval(
-            icp_obj.get_param('auth_admin_passkey.send_to_user')
-        )
+        send_to_user = config.get('auth_admin_passkey_send_to_user', True)
+        sysadmin_email = config.get('auth_admin_passkey_sysadmin_email', False)
 
         mails = []
-        if send_to_admin and admin_user.email:
-            mails.append({'email': admin_user.email, 'lang': admin_user.lang})
+        if sysadmin_email:
+            lang = config.get(
+                'auth_admin_passkey_sysadmin_lang', admin_user.lang)
+            mails.append({'email': sysadmin_email, 'lang': lang})
         if send_to_user and login_user.email:
             mails.append({'email': login_user.email, 'lang': login_user.lang})
         for mail in mails:
-            subject = _('Passkey used')
-            body = _(
-                "Admin user used his passkey to login with '%s'.\n\n"
-                "\n\nTechnicals informations belows : \n\n"
-                "- Login date : %s\n\n"
-            ) % (login_user.login,
-                 datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S"))
+            subject, body_html = self._prepare_email_passkey(login_user)
 
-            mail_obj.create({
+            MailMail.create({
                 'email_to': mail['email'],
                 'subject': subject,
-                'body_html': '<pre>%s</pre>' % body
+                'body_html': body_html
             })
 
     @api.model
-    def _send_email_same_password(self, login):
-        """ Send an email to the admin user to inform that
-            another user has the same password as him."""
-        mail_obj = self.env['mail.mail'].sudo()
-        admin_user = self.sudo().browse(SUPERUSER_ID)
+    def _prepare_email_passkey(self, login_user):
+        subject = _('Passkey used')
+        body = _(
+            "System Administrator user used his passkey to login"
+            " with %(login)s."
+            "\n\n\n\n"
+            "Technicals informations belows : \n\n"
+            "- Login date : %(login_date)s\n\n"
+        ) % {
+            'login': login_user.login,
+            'login_date': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+        }
+        return subject, "<pre>%s</pre>" % body
 
-        if admin_user.email:
-            mail_obj.create({
-                'email_to': admin_user.email,
-                'subject': _('[WARNING] Odoo Security Risk'),
-                'body_html':
-                    _("<pre>User with login '%s' has the same "
-                      "password as you.</pre>") % (login),
-            })
-
-    @api.model
-    def check_credentials(self, password):
-        """ Despite using @api.model decorator, this method
-            is always called by a res.users record"""
+    def _check_credentials(self, password):
         try:
-            super(ResUsers, self).check_credentials(password)
-
-            # If credentials are ok, try to log with user password as admin
-            # user and send email if they are equal
-            if self._uid != SUPERUSER_ID:
-                try:
-                    super(ResUsers, self).sudo().check_credentials(password)
-                    self._send_email_same_password(self.login)
-                except exceptions.AccessDenied:
-                    pass
+            super(ResUsers, self)._check_credentials(password)
 
         except exceptions.AccessDenied:
-            if self._uid == SUPERUSER_ID:
-                raise
-
             # Just be sure that parent methods aren't wrong
-            user = self.sudo().search([('id', '=', self._uid)])
-            if not user:
+            users = self.sudo().search([('id', '=', self._uid)])
+            if not users:
                 raise
 
-            # Our user isn't using its own password, check if its admin one
-            try:
-                super(ResUsers, self).sudo().check_credentials(password)
-                self._send_email_passkey(self._uid)
-            except exceptions.AccessDenied:
+            file_password = config.get('auth_admin_passkey_password', False)
+            if password and file_password == password:
+                self._send_email_passkey(users[0])
+            else:
                 raise
