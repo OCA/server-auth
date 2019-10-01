720282f5ba32331eb1feb289e9beadff8330be61
Robert Rübner <rruebner@bloopark.de>
2019-10-01 15:23:41 +0200

    [MIG] auth_session_timeout: Migration to 13.0

--- auth_session_timeout/__manifest__.py
+++ auth_session_timeout/__manifest__.py
@@ -13,7 +13,7 @@
     'maintainer': 'Odoo Community Association (OCA)',
     'website': "http://www.github.com/OCA/server-auth",
     'category': 'Tools',
-    'version': '12.0.1.0.0',
+    'version': '13.0.1.0.0',
     'license': 'AGPL-3',
     'data': [
         'data/ir_config_parameter_data.xml'
