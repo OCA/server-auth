236ec896e792490d45c1deba6ed2f6615b0c5f3f
Alexandre Díaz <alexandre.diaz@tecnativa.com>
2019-12-18 14:42:41 +0100

    users_ldap_mail: Migration to 12.0

--- users_ldap_mail/__manifest__.py
+++ users_ldap_mail/__manifest__.py
@@ -1,9 +1,9 @@
-# © Daniel Reis (https://launchpad.com/~dreis-pt)
+# Copyright Daniel Reis (https://launchpad.com/~dreis-pt)
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 {
     'name': "LDAP mapping for user name and e-mail",
-    'version': "11.0.1.0.0",
+    'version': "12.0.1.0.0",
     'depends': ["auth_ldap"],
     'author': "Daniel Reis,"
               "Odoo Community Association (OCA)",
--- users_ldap_mail/models/__init__.py
+++ users_ldap_mail/models/__init__.py
@@ -1,5 +1,4 @@
-# © Daniel Reis (https://launchpad.com/~dreis-pt)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# Copyright Daniel Reis (https://launchpad.com/~dreis-pt)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import users_ldap_model
-
