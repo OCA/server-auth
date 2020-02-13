0649f2cbd19542422c4e3f3ddfc29965f8549c76
Khalid <m.khalid@serpentcs.com>
2020-02-13 12:42:08 +0530

    [13.0][MIG] auth_ldap_user_pattern

--- auth_ldap_user_pattern/models/res_company_ldap.py
+++ auth_ldap_user_pattern/models/res_company_ldap.py
@@ -2,27 +2,24 @@
 # Copyright (C) 2019 Open Source Integrators
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-from odoo import api, fields, models
+from odoo import fields, models
 
 
 class CompanyLDAP(models.Model):
     _inherit = "res.company.ldap"
 
-    user_pattern = fields.Char(
-        string='User Pattern',
-        size=128
-    )
+    user_pattern = fields.Char(string="User Pattern", size=128)
 
     def map_ldap_attributes(self, conf, login, ldap_entry):
         res = super(CompanyLDAP, self).map_ldap_attributes(
-            conf=conf, login=login, ldap_entry=ldap_entry)
-        res.update({'is_ldap_user': True})
+            conf=conf, login=login, ldap_entry=ldap_entry
+        )
+        res.update({"is_ldap_user": True})
         return res
 
-    @api.multi
     def get_ldap_dicts(self):
         res = super(CompanyLDAP, self).get_ldap_dicts()
         for value in res:
-            user_pattern = self.sudo().browse(value['id']).user_pattern
-            value.update({'user_pattern': user_pattern})
+            user_pattern = self.sudo().browse(value["id"]).user_pattern
+            value.update({"user_pattern": user_pattern})
         return res
--- auth_ldap_user_pattern/models/res_users.py
+++ auth_ldap_user_pattern/models/res_users.py
@@ -4,52 +4,54 @@
 
 import re
 
-from odoo import api, fields, models, registry, SUPERUSER_ID, _
+from odoo import SUPERUSER_ID, _, api, fields, models, registry
 from odoo.exceptions import UserError
 
 
 class Users(models.Model):
     _inherit = "res.users"
 
-    is_ldap_user = fields.Boolean(
-        string='Is LDAP User',
-    )
+    is_ldap_user = fields.Boolean(string="Is LDAP User")
 
     @classmethod
     def _login(cls, db, login, password):
-        """ Overload _login to check login string matches with pattern
-        before passing to the super """
+        """Overload _login to check login string matches with pattern
+        before passing to the super"""
         login = login.lower()
         with registry(db).cursor() as cr:
             env = api.Environment(cr, SUPERUSER_ID, {})
-            Ldap = env['res.company.ldap']
-            for conf in Ldap.get_ldap_dicts():
-                if conf['user_pattern']:
-                    pattern = re.compile(conf['user_pattern'])
+            ldap = env["res.company.ldap"]
+            for conf in ldap.get_ldap_dicts():
+                if conf["user_pattern"]:
+                    pattern = re.compile(conf["user_pattern"])
                     if pattern.match(login):
                         # Bypass Standard Odoo authentication and only check
                         # user id
-                        entry = Ldap.authenticate(conf, login, password)
+                        entry = ldap.authenticate(conf, login, password)
                         if entry:
-                            user_id = Ldap.get_or_create_user(
-                                conf, login, entry)
+                            user_id = ldap.get_or_create_user(conf, login, entry)
                             if user_id:
                                 return user_id
         return super(Users, cls)._login(db, login, password)
 
 
 class ChangePasswordUser(models.TransientModel):
-    _inherit = 'change.password.user'
+    _inherit = "change.password.user"
 
-    @api.multi
     def change_password_button(self):
         for line in self:
             if not line.new_passwd:
-                raise UserError(_("Before clicking on 'Change Password', "
-                                  "you have to write a new password."))
+                raise UserError(
+                    _(
+                        "Before clicking on 'Change Password', "
+                        "you have to write a new password."
+                    )
+                )
             if line.user_id.is_ldap_user and not line.user_id.has_group(
-                    'auth_ldap_user_pattern.group_ldap_password_change'):
-                raise UserError(_("Sorry, You can not change password for "
-                                  "LDAP User!"))
+                "auth_ldap_user_pattern.group_ldap_password_change"
+            ):
+                raise UserError(
+                    _("Sorry, You can not change password for " "LDAP User!")
+                )
         res = super(ChangePasswordUser, self).change_password_button()
         return res
