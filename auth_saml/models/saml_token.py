aecaa01c78f1a45673a71c65d49c26c0b7f39c9c
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-03-19 13:47:17 +0000

    [MIG][REF] General cleanup for migration
    - Default behavior is now to allow password and SAML together.
      Otherwise, users could keep getting their passwords removed without warning.
    - General cleanup.
    - Remove relations to field `password_crypt` because in v12 the
      `password` field is always encrypted instead.
    
    Co-Authored-By: Alexandre Díaz <alexandre.diaz@tecnativa.com>

--- auth_saml/models/saml_token.py
+++ auth_saml/models/saml_token.py
@@ -1,11 +1,7 @@
 # Copyright (C) 2010-2016 XCG Consulting <http://odoo.consulting>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
-import logging
-
 from odoo import fields, models
-
-_logger = logging.getLogger(__name__)
 
 
 class SamlToken(models.Model):
@@ -16,15 +12,19 @@ class SamlToken(models.Model):
     saml_provider_id = fields.Many2one(
         'auth.saml.provider',
         string='SAML Provider that issued the token',
+        required=True,
     )
     user_id = fields.Many2one(
         'res.users',
         string="User",
+        required=True,
         # we want the token to be destroyed if the corresponding res.users
         # is deleted
-        ondelete="cascade"
+        ondelete="cascade",
+        index=True,
     )
     saml_access_token = fields.Char(
         'Current SAML token for this user',
+        required=True,
         help="The current SAML token in use",
     )
