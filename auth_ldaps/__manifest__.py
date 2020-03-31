c57168793dc35c72319e82bbca872365f94a219c
Bhavesh Odedra <bodedra@ursainfosystems.com>
2020-03-31 23:34:56 +0530

    [IMP] code with pre-commit

--- auth_ldaps/__manifest__.py
+++ auth_ldaps/__manifest__.py
@@ -3,22 +3,17 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 {
-    'name': 'LDAPS authentication',
-    'version': '13.0.1.0.0',
-    'category': 'Tools',
-    'website': 'https://github.com/OCA/server-auth',
-    'author':
-        'Brainbean Apps (https://brainbeanapps.com), '
-        'Creu Blanca, '
-        'Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'application': False,
-    'summary': 'Allows to use LDAP over SSL authentication',
-    'depends': [
-        'auth_ldap',
-    ],
-    'data': [
-        'views/res_company_ldap_views.xml',
-    ],
+    "name": "LDAPS authentication",
+    "version": "13.0.1.0.0",
+    "category": "Tools",
+    "website": "https://github.com/OCA/server-auth",
+    "author": "Brainbean Apps (https://brainbeanapps.com), "
+    "Creu Blanca, "
+    "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "application": False,
+    "summary": "Allows to use LDAP over SSL authentication",
+    "depends": ["auth_ldap", ],
+    "data": ["views/res_company_ldap_views.xml", ],
 }
