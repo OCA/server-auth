45c56e90a62b52b031dd931c12ff4ed9cc77c028
Bhavesh Odedra <bodedra@ursainfosystems.com>
2020-04-01 13:04:19 +0530

    [IMP] auth_ldaps: black, isort, prettier

--- auth_ldaps/models/res_company_ldap.py
+++ auth_ldaps/models/res_company_ldap.py
@@ -3,6 +3,7 @@
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl.html).
 
 import logging
+
 from odoo import fields, models
 
 _logger = logging.getLogger(__name__)
@@ -14,35 +15,30 @@ except (ImportError) as err:
 
 
 class CompanyLDAP(models.Model):
-    _inherit = 'res.company.ldap'
-    _description = 'Company LDAP configuration'
+    _inherit = "res.company.ldap"
+    _description = "Company LDAP configuration"
 
-    is_ssl = fields.Boolean(string='Use LDAPS', default=False)
+    is_ssl = fields.Boolean(string="Use LDAPS", default=False)
     skip_cert_validation = fields.Boolean(
-        string='Skip certificate validation',
-        default=False
+        string="Skip certificate validation", default=False
     )
 
     def _get_ldap_dicts(self):
         res = super()._get_ldap_dicts()
         for rec in res:
-            ldap = self.sudo().browse(rec['id'])
-            rec['is_ssl'] = ldap.is_ssl or False
-            rec['skip_cert_validation'] = ldap.skip_cert_validation or False
+            ldap = self.sudo().browse(rec["id"])
+            rec["is_ssl"] = ldap.is_ssl or False
+            rec["skip_cert_validation"] = ldap.skip_cert_validation or False
         return res
 
     def _connect(self, conf):
-        if conf['is_ssl']:
-            uri = 'ldaps://%s:%d' % (
-                conf['ldap_server'], conf['ldap_server_port'])
+        if conf["is_ssl"]:
+            uri = "ldaps://%s:%d" % (conf["ldap_server"], conf["ldap_server_port"])
             connection = ldap.initialize(uri)
-            if conf['skip_cert_validation']:
-                connection.set_option(
-                    ldap.OPT_X_TLS_REQUIRE_CERT,
-                    ldap.OPT_X_TLS_ALLOW
-                )
+            if conf["skip_cert_validation"]:
+                connection.set_option(ldap.OPT_X_TLS_REQUIRE_CERT, ldap.OPT_X_TLS_ALLOW)
             connection.set_option(ldap.OPT_X_TLS_NEWCTX, 0)
-            if conf['ldap_tls']:
+            if conf["ldap_tls"]:
                 connection.start_tls_s()
             return connection
         return super()._connect(conf)
