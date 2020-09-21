45843d622b90a3f1114381703d64db022407f098
Jairo Llopis <jairo.llopis@tecnativa.com>
2020-09-21 12:41:06 +0200

    [MIG] auth_oauth_multi_token: v13
    Nothing special. Just making linters happy and splitting the readme.
    
    @Tecnativa TT25619

--- auth_oauth_multi_token/models/auth_oauth_multi_token.py
+++ auth_oauth_multi_token/models/auth_oauth_multi_token.py
@@ -1,5 +1,6 @@
 # Copyright 2016 Florent de Labarre
 # Copyright 2017 Camptocamp
+# Copyright 2021 ACSONE SA/NV
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, fields, models
@@ -12,15 +13,12 @@ class AuthOauthMultiToken(models.Model):
     _description = "OAuth2 token"
     _order = "id desc"
 
-    EMPTY_OAUTH_TOKEN = "****************************"
-
     oauth_access_token = fields.Char(
         string="OAuth Access Token", readonly=True, copy=False
     )
     user_id = fields.Many2one(
         comodel_name="res.users", string="User", required=True, readonly=True
     )
-    active_token = fields.Boolean("Active", readonly=True, copy=False)
 
     @api.model
     def create(self, vals):
@@ -30,13 +28,12 @@ class AuthOauthMultiToken(models.Model):
         return token
 
     @api.model
-    def _oauth_user_tokens(self, user_id, active=True):
+    def _oauth_user_tokens(self, user_id):
         """Retrieve tokens for given user.
 
         :param user_id: Odoo ID of the user
-        :param active: retrieve active or inactive tokens
         """
-        return self.search([("user_id", "=", user_id), ("active_token", "=", active)])
+        return self.search([("user_id", "=", user_id)])
 
     def _oauth_validate_multi_token(self):
         """Check current user's token and clear them if max number reached."""
@@ -44,10 +41,4 @@ class AuthOauthMultiToken(models.Model):
         max_token = self.user_id.oauth_access_max_token
         if user_tokens and len(user_tokens) > max_token:
             # clear last token
-            user_tokens[max_token - 1]._oauth_clear_token()
-
-    def _oauth_clear_token(self):
-        """Disable current token records."""
-        self.write(
-            {"oauth_access_token": self.EMPTY_OAUTH_TOKEN, "active_token": False}
-        )
+            user_tokens[max_token - 1].unlink()
