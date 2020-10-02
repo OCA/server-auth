d7bb08e85565555086d786e16c197b46d807d398
Ruchir Shukla <ruchir@bizzappdev.com>
2020-10-02 15:05:08 +0530

    [IMP] : black, isort, prettier

--- auth_admin_passkey/tests/test_auth_admin_passkey.py
+++ auth_admin_passkey/tests/test_auth_admin_passkey.py
@@ -3,8 +3,8 @@
 # License AGPL-3 - See http://www.gnu.org/licenses/agpl-3.0.html
 
 from odoo import exceptions
-from odoo.tools import config
 from odoo.tests import common
+from odoo.tools import config
 
 
 @common.post_install(True)
@@ -14,21 +14,23 @@ class TestAuthAdminPasskey(common.TransactionCase):
     def setUp(self):
         super(TestAuthAdminPasskey, self).setUp()
 
-        self.ResUsers = self.env['res.users']
+        self.ResUsers = self.env["res.users"]
 
         self.db = self.env.cr.dbname
 
-        self.user_login = 'auth_admin_passkey_user'
-        self.user_password = 'Auth_admin_passkey_password*1'
-        self.sysadmin_passkey = 'SysAdminPasskeyPa$$w0rd'
-        self.bad_password = 'Bad_password*000001'
-        self.bad_login = 'bad_login'
+        self.user_login = "auth_admin_passkey_user"
+        self.user_password = "Auth_admin_passkey_password*1"
+        self.sysadmin_passkey = "SysAdminPasskeyPa$$w0rd"
+        self.bad_password = "Bad_password*000001"
+        self.bad_login = "bad_login"
 
-        user = self.ResUsers.create({
-            'login': self.user_login,
-            'password': self.user_password,
-            'name': 'auth_admin_passkey User'
-        })
+        user = self.ResUsers.create(
+            {
+                "login": self.user_login,
+                "password": self.user_password,
+                "name": "auth_admin_passkey User",
+            }
+        )
         self.user = user.sudo(user)
 
     def test_01_normal_login_succeed(self):
@@ -40,14 +42,13 @@ class TestAuthAdminPasskey(common.TransactionCase):
 
     def test_03_normal_login_passkey_fail(self):
         # This should failed, because feature is disabled
-        config['auth_admin_passkey_password'] = False
+        config["auth_admin_passkey_password"] = False
         with self.assertRaises(exceptions.AccessDenied):
-            self.user._check_credentials(
-                self.sysadmin_passkey)
+            self.user._check_credentials(self.sysadmin_passkey)
 
     def test_04_normal_login_passkey_succeed(self):
         # This should succeed, because feature is enabled
-        config['auth_admin_passkey_password'] = self.sysadmin_passkey
+        config["auth_admin_passkey_password"] = self.sysadmin_passkey
         self.user._check_credentials(self.sysadmin_passkey)
 
     def test_05_passkey_login_passkey_succeed(self):
@@ -55,4 +56,5 @@ class TestAuthAdminPasskey(common.TransactionCase):
         Test the correct behaviour of login with 'bad_login' / 'admin'"""
         with self.assertRaises(exceptions.AccessDenied):
             self.ResUsers.authenticate(
-                self.db, self.bad_login, self.sysadmin_passkey, {})
+                self.db, self.bad_login, self.sysadmin_passkey, {}
+            )
