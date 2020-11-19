a85c04536148628adcf1ac9e5849d68716a37d38
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-11-19 18:15:08 +0100

    Adapt to OCA standards
    update manifest, add README, update requirements.txt

--- auth_oidc/__manifest__.py
+++ auth_oidc/__manifest__.py
@@ -1,19 +1,14 @@
 # -*- coding: utf-8 -*-
-# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# Copyright 2016 ICTSTUDIO <http://www.ictstudio.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
     'name': 'Authentication OpenID Connect',
     'version': '10.0.1.0.0',
     'license': 'AGPL-3',
-    'author': 'ICTSTUDIO, André Schenkels',
-    'website': 'https://www.ictstudio.eu',
-    'description': """
-Allow users to login through OpenID Connect Provider.
-=====================================================
-- Keycloak with ClientID and Secret + Implicit Flow
-
-""",
+    'author': 'ICTSTUDIO, André Schenkels, Odoo Community Association (OCA)',
+    'website': 'https://github.com/OCA/server-auth',
+    'summary': "Allow users to login through OpenID Connect Provider",
     'external_dependencies': {'python': [
         'jose',
         'cryptography'
