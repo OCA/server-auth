ef5d7a07f31452d394b6ae730935ee898b665cba
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-11-19 18:15:08 +0100

    Adapt to OCA standards
    update manifest, add README, update requirements.txt

--- auth_oidc/controllers/main.py
+++ auth_oidc/controllers/main.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-# Copyright© 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# Copyright 2016 ICTSTUDIO <http://www.ictstudio.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 import werkzeug.utils
@@ -15,11 +15,18 @@ class OpenIDLogin(OAuthLogin):
         for provider in providers:
             if provider.get('flow') == 'id_token':
                 provider['nonce'] = uuid.uuid1().hex
-                params = werkzeug.url_decode(provider['auth_link'].split('?')[-1])
+                params = werkzeug.url_decode(
+                    provider['auth_link'].split('?')[-1]
+                )
                 params.pop('response_type')
                 params.update(dict(response_type='id_token',
                                    nonce=provider['nonce']))
                 if provider.get('scope'):
                     params['scope'] = provider['scope']
-                provider['auth_link'] = "%s?%s" % (provider['auth_endpoint'], werkzeug.url_encode(params))
+                provider['auth_link'] = (
+                    "%s?%s" % (
+                        provider['auth_endpoint'],
+                        werkzeug.url_encode(params)
+                    )
+                )
         return providers
