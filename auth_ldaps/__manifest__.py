f7de3de5937781c2a49b5f88b504c3a7c4d3461a
Alexandre Fayolle <alexandre.fayolle@camptocamp.com>
2020-11-30 12:21:38 +0100

    [MIG][14.0] auth_ldaps

--- auth_ldaps/__manifest__.py
+++ auth_ldaps/__manifest__.py
@@ -5,7 +5,7 @@
 
 {
     "name": "LDAPS authentication",
-    "version": "13.0.1.0.1",
+    "version": "14.0.1.0.0",
     "category": "Tools",
     "website": "https://github.com/OCA/server-auth",
     "author": "CorporateHub, " "Creu Blanca, " "Odoo Community Association (OCA)",
