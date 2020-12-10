6e7fc2be0094bafe9e567f1a1b8c9abd4b27ab1a
Loïc Faure-Lacroix <lamerstar@gmail.com>
2020-12-10 09:19:45 -0500

    Reformat password_security to match tests
    Set requirements in the root of projects

--- password_security/exceptions.py
+++ password_security/exceptions.py
@@ -6,6 +6,7 @@ from odoo.exceptions import Warning as UserError
 
 class PassError(UserError):
     """ Example: When you try to create an insecure password."""
+
     def __init__(self, msg):
         self.message = msg
         super(PassError, self).__init__(msg)
