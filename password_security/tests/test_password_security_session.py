d65a4ea2510b8daa5c63cc124a916e234bb3841d
Loïc Faure-Lacroix <lamerstar@gmail.com>
2020-12-10 09:19:45 -0500

    Reformat password_security to match tests
    Set requirements in the root of projects

--- password_security/tests/test_password_security_session.py
+++ password_security/tests/test_password_security_session.py
@@ -1,16 +1,15 @@
 # Copyright 2016 LasLabs Inc.
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-import mock
-
 from contextlib import contextmanager
+
+import mock
 
 from odoo.tests.common import TransactionCase
 
 from ..controllers import main
 
-
-IMPORT = 'odoo.addons.password_security.controllers.main'
+IMPORT = "odoo.addons.password_security.controllers.main"
 
 
 class EndTestException(Exception):
@@ -18,40 +17,35 @@ class EndTestException(Exception):
 
 
 class TestPasswordSecuritySession(TransactionCase):
-
     def setUp(self):
         super(TestPasswordSecuritySession, self).setUp()
         self.PasswordSecuritySession = main.PasswordSecuritySession
         self.password_security_session = self.PasswordSecuritySession()
-        self.passwd = 'I am a password!'
+        self.passwd = "I am a password!"
         self.fields = [
-            {'name': 'new_password', 'value': self.passwd},
+            {"name": "new_password", "value": self.passwd},
         ]
 
     @contextmanager
     def mock_assets(self):
         """ It mocks and returns assets used by this controller """
-        with mock.patch('%s.request' % IMPORT) as request:
+        with mock.patch("%s.request" % IMPORT) as request:
             yield {
-                'request': request,
+                "request": request,
             }
 
     def test_change_password_check(self):
         """ It should check password on request user """
         with self.mock_assets() as assets:
-            check_password = assets['request'].env.user._check_password
+            check_password = assets["request"].env.user._check_password
             check_password.side_effect = EndTestException
             with self.assertRaises(EndTestException):
                 self.password_security_session.change_password(self.fields)
-            check_password.assert_called_once_with(
-                self.passwd,
-            )
+            check_password.assert_called_once_with(self.passwd,)
 
     def test_change_password_return(self):
         """ It should return result of super """
         with self.mock_assets():
-            with mock.patch.object(main.Session, 'change_password') as chg:
-                res = self.password_security_session.change_password(
-                    self.fields
-                )
+            with mock.patch.object(main.Session, "change_password") as chg:
+                res = self.password_security_session.change_password(self.fields)
                 self.assertEqual(chg(), res)
