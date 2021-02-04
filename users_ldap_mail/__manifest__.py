5235302a3f6bc00843813bed8b3fcf765742ab8c
João Marques <joao.marques@tecnativa.com>
2021-02-04 14:23:45 +0000

    [IMP] users_ldap_mail: black, isort, prettier

--- users_ldap_mail/__manifest__.py
+++ users_ldap_mail/__manifest__.py
@@ -2,15 +2,12 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
 
 {
-    'name': "LDAP mapping for user name and e-mail",
-    'version': "12.0.1.0.0",
-    'depends': ["auth_ldap"],
-    'author': "Daniel Reis,"
-              "Odoo Community Association (OCA)",
-    'license': 'AGPL-3',
-    'category': "Tools",
-    'data': [
-        'views/res_company_ldap.xml',
-    ],
-    'installable': True,
+    "name": "LDAP mapping for user name and e-mail",
+    "version": "12.0.1.0.0",
+    "depends": ["auth_ldap"],
+    "author": "Daniel Reis," "Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Tools",
+    "data": ["views/res_company_ldap.xml",],
+    "installable": True,
 }
