4b62e000d17f1f2084f3a0982542358fdfc51e92
fkantelberg <florian.kantelberg@initos.com>
2021-03-12 14:02:30 +0100

    Add vault and vault_share module

---
+++ vault/models/__init__.py
@@ -0,0 +1,12 @@
+# © 2021 Florian Kantelberg - initOS GmbH
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from . import (
+    abstract_vault,
+    res_users,
+    vault,
+    vault_entry,
+    vault_field,
+    vault_inbox,
+    vault_right,
+)
