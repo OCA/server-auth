f944ec1190bcd15c88de49849b746ce89b6cc42a
fkantelberg <florian.kantelberg@initos.com>
2021-03-12 14:44:48 +0100

    Run pre-commit

--- vault_share/static/src/js/vault_utils.js
+++ vault_share/static/src/js/vault_utils.js
@@ -2,18 +2,18 @@
 // License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 odoo.define("vault.share.utils", function (require) {
-  "use strict";
+    "use strict";
 
-  const PinSize = 5;
+    const PinSize = 5;
 
-  var utils = require("vault.utils");
+    var utils = require("vault.utils");
 
-  function generate_pin(pin_size) {
-    return utils.generate_secret(pin_size, "0123456789");
-  }
+    function generate_pin(pin_size) {
+        return utils.generate_secret(pin_size, "0123456789");
+    }
 
-  return {
-    PinSize: PinSize,
-    generate_pin: generate_pin,
-  };
+    return {
+        PinSize: PinSize,
+        generate_pin: generate_pin,
+    };
 });
