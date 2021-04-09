e5380275fcad3593694952c9e054515a4e3e8394
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-04-09 16:07:29 +0200

    [MIG] auth_oidc: update dotfiles

--- auth_oidc/__manifest__.py
+++ auth_oidc/__manifest__.py
@@ -1,20 +1,14 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ICTSTUDIO <http://www.ictstudio.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 {
-    'name': 'Authentication OpenID Connect',
-    'version': '10.0.1.0.0',
-    'license': 'AGPL-3',
-    'author': 'ICTSTUDIO, André Schenkels, Odoo Community Association (OCA)',
-    'website': 'https://github.com/OCA/server-auth',
-    'summary': "Allow users to login through OpenID Connect Provider",
-    'external_dependencies': {'python': [
-        'jose',
-        'cryptography'
-    ]},
-    'depends': ['auth_oauth'],
-    'data': [
-        'views/auth_oauth_provider.xml',
-    ],
+    "name": "Authentication OpenID Connect",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ICTSTUDIO, André Schenkels, Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-auth",
+    "summary": "Allow users to login through OpenID Connect Provider",
+    "external_dependencies": {"python": ["jose", "cryptography"]},
+    "depends": ["auth_oauth"],
+    "data": ["views/auth_oauth_provider.xml"],
 }
