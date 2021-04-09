ebca73d8d2c42305ccd0e6225bb9d97514cb6187
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-04-10 01:43:57 +0200

    auth_oidc: add authorization code flow

--- auth_oidc/controllers/main.py
+++ auth_oidc/controllers/main.py
@@ -2,25 +2,34 @@
 # Copyright 2021 ACSONE SA/NV <https://acsone.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
+import logging
 import uuid
 
 import werkzeug.utils
 
 from odoo.addons.auth_oauth.controllers.main import OAuthLogin
 
+_logger = logging.getLogger(__name__)
+
 
 class OpenIDLogin(OAuthLogin):
     def list_providers(self):
         providers = super(OpenIDLogin, self).list_providers()
         for provider in providers:
-            if provider.get("flow") == "id_token":
-                provider["nonce"] = uuid.uuid1().hex  # TODO Better nonce
+            flow = provider.get("flow")
+            if flow in ("id_token", "id_token_code"):
                 params = werkzeug.url_decode(provider["auth_link"].split("?")[-1])
-                params.pop("response_type")
-                params.update(
-                    dict(response_type="id_token token", nonce=provider["nonce"])
-                )
+                params["nonce"] = uuid.uuid1().hex  # TODO Better nonce
+                if flow == "id_token":
+                    # https://openid.net/specs/openid-connect-core-1_0.html
+                    # #ImplicitAuthRequest
+                    params["response_type"] = "id_token token"
+                elif flow == "id_token_code":
+                    # https://openid.net/specs/openid-connect-core-1_0.html#AuthRequest
+                    params["response_type"] = "code"
                 if provider.get("scope"):
+                    if "openid" not in provider["scope"].split():
+                        _logger.error("openid connect scope must contain 'openid'")
                     params["scope"] = provider["scope"]
                 provider["auth_link"] = "{}?{}".format(
                     provider["auth_endpoint"], werkzeug.url_encode(params)
