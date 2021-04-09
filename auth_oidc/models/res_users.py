dbbd9bbba1dd2efd7f1cb3c542eb9ba3c5f19f82
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-04-09 20:03:21 +0200

    auth_oidc: simplify and use "id_token token"
    Avoid replacing the access token by the id token.
    This may cause confusion.
    Copy a little piece of code from auth_oauth() method,
    to make the code easier to follow, and prepare for
    implementing the authorization code flow.

--- auth_oidc/models/res_users.py
+++ auth_oidc/models/res_users.py
@@ -1,24 +1,29 @@
 # Copyright 2016 ICTSTUDIO <http://www.ictstudio.eu>
+# Copyright 2021 ACSONE SA/NV <https://acsone.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
 from odoo import api, models
+from odoo.exceptions import AccessDenied
 
 
 class ResUsers(models.Model):
     _inherit = "res.users"
 
     @api.model
-    def _auth_oauth_validate(self, provider, access_token):
-        """ return the validation data corresponding to the access token """
-        oauth_provider = self.env["auth.oauth.provider"].browse(provider)
-        if oauth_provider.flow == "id_token":
-            return oauth_provider._parse_id_token(access_token)
-        else:
-            return super(ResUsers, self)._auth_oauth_validate()
-
-    @api.model
     def auth_oauth(self, provider, params):
-        id_token = params.get("id_token")
-        if id_token:
-            params["access_token"] = id_token
-        return super(ResUsers, self).auth_oauth(provider, params)
+        oauth_provider = self.env["auth.oauth.provider"].browse(provider)
+        if oauth_provider.flow != "id_token":
+            return super(ResUsers, self).auth_oauth(provider, params)
+
+        access_token = params.get('access_token')
+        id_token = params.get('id_token')
+        validation = oauth_provider._parse_id_token(id_token, access_token)
+        # required check
+        if not validation.get('user_id'):
+            raise AccessDenied()
+        # retrieve and sign in user
+        login = self._auth_oauth_signin(provider, validation, params)
+        if not login:
+            raise AccessDenied()
+        # return user credentials
+        return (self.env.cr.dbname, login, access_token)
