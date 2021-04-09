d8f6850fe3bfcccb91a6e3d5f3a91e1c1b01801b
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-04-09 16:07:29 +0200

    [MIG] auth_oidc: update dotfiles

--- auth_oidc/models/auth_oauth_provider.py
+++ auth_oidc/models/auth_oauth_provider.py
@@ -1,57 +1,58 @@
-# -*- coding: utf-8 -*-
 # Copyright 2016 ICTSTUDIO <http://www.ictstudio.eu>
 # License: AGPL-3.0 or later (http://www.gnu.org/licenses/agpl)
 
-from odoo import models, fields, api
-import urllib2
 import json
 import logging
+import urllib2
+
+from odoo import api, fields, models
 
 try:
     from jose import jwt
 except ImportError:
-    logging.getLogger(__name__).debug('jose library not installed')
+    logging.getLogger(__name__).debug("jose library not installed")
 
 
 class AuthOauthProvider(models.Model):
-    _inherit = 'auth.oauth.provider'
+    _inherit = "auth.oauth.provider"
 
-    flow = fields.Selection([
-        ('access_token', 'OAuth2'),
-        ('id_token', 'OpenID Connect')
-    ],
-        string='Auth Flow',
+    flow = fields.Selection(
+        [("access_token", "OAuth2"), ("id_token", "OpenID Connect")],
+        string="Auth Flow",
         required=True,
-        default='access_token')
+        default="access_token",
+    )
 
     token_map = fields.Char(
         help="Some Oauth providers don't map keys in their responses "
-             "exactly as required.  It is important to ensure user_id and "
-             "email at least are mapped. For OpenID Connect user_id is "
-             "the sub key in the standard.")
+        "exactly as required.  It is important to ensure user_id and "
+        "email at least are mapped. For OpenID Connect user_id is "
+        "the sub key in the standard."
+    )
 
     validation_endpoint = fields.Char(
-        help='For OpenID Connect this should be the location for public keys ')
+        help="For OpenID Connect this should be the location for public keys "
+    )
 
     @api.model
     def _get_key(self, header):
-        if self.flow != 'id_token':
+        if self.flow != "id_token":
             return False
         f = urllib2.urlopen(self.validation_endpoint)
         response = json.loads(f.read())
         rsa_key = {}
         for key in response["keys"]:
-            if key["kid"] == header.get('kid'):
+            if key["kid"] == header.get("kid"):
                 rsa_key = key
         return rsa_key
 
     @api.model
     def map_token_values(self, res):
         if self.token_map:
-            for pair in self.token_map.split(' '):
-                from_key, to_key = pair.split(':')
+            for pair in self.token_map.split(" "):
+                from_key, to_key = pair.split(":")
                 if to_key not in res:
-                    res[to_key] = res.get(from_key, '')
+                    res[to_key] = res.get(from_key, "")
         return res
 
     @api.multi
@@ -59,11 +60,14 @@ class AuthOauthProvider(models.Model):
         self.ensure_one()
         res = {}
         header = jwt.get_unverified_header(id_token)
-        res.update(jwt.decode(
-            id_token,
-            self._get_key(header),
-            algorithms=['RS256'],
-            audience=self.client_id))
+        res.update(
+            jwt.decode(
+                id_token,
+                self._get_key(header),
+                algorithms=["RS256"],
+                audience=self.client_id,
+            )
+        )
 
         res.update(self.map_token_values(res))
         return res
