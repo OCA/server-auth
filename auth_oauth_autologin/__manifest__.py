614e4c9fc31415e736ff51662d37794dbe03d70d
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-04-20 11:54:11 +0200

    [ADD] auth_oauth_autologin

---
+++ auth_oauth_autologin/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2021 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Auth Oauth Autologin",
+    "summary": """
+        Automatically redirect to the OAuth provider for login""",
+    "version": "13.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "maintainers": ["sbidoul"],
+    "website": "https://github.com/OCA/server-auth",
+    "depends": ["auth_oauth"],
+    "data": ["views/auth_oauth_provider.xml"],
+    "demo": [],
+}
