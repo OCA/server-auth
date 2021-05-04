d637b768cf33f000a6120a361fa9085441f88e46
Laurent Mignon (ACSONE) <laurent.mignon@acsone.eu>
2021-05-04 12:05:42 +0200

    [FIX] auth_api_key: Fix migration script to work with inactive users

--- auth_api_key/migrations/10.0.2.0.0/post-migrate.py
+++ auth_api_key/migrations/10.0.2.0.0/post-migrate.py
@@ -20,7 +20,8 @@ def migrate(cr, version):
                 login_name = serv_config.get(section, "user")
                 name = section.replace("api_key_", "")
                 key = "<set from server environment>"
-                user = env["res.users"].search([("login", "=", login_name)])
+                user = env["res.users"].with_context(
+                    active_test=False).search([("login", "=", login_name)])
                 env["auth.api.key"].create(
                     {"name": name, "key": key, "user_id": user.id}
                 )
