56d2a603d90bf011ad84118a036fbbf3863aa219
Karl Southern <karl@theangryangel.co.uk>
2021-06-25 09:44:34 +0100

    [IMP] auth_saml: black, isort, prettier

--- auth_saml/wizards/res_config_settings.py
+++ auth_saml/wizards/res_config_settings.py
@@ -5,23 +5,24 @@ from odoo import api, fields, models
 
 
 class ResConfigSettings(models.TransientModel):
-    _inherit = 'res.config.settings'
+    _inherit = "res.config.settings"
 
     allow_saml_uid_and_internal_password = fields.Boolean(
-        string='Allow SAML users to posess an Odoo password '
-               '(warning: decreases security)'
+        string="Allow SAML users to posess an Odoo password "
+        "(warning: decreases security)"
     )
 
     @api.model
     def get_values(self):
         res = super().get_values()
-        res["allow_saml_uid_and_internal_password"] = \
-            self.env["res.users"]._allow_saml_and_password()
+        res["allow_saml_uid_and_internal_password"] = self.env[
+            "res.users"
+        ]._allow_saml_and_password()
         return res
 
     def set_values(self):
-        self.env['ir.config_parameter'].sudo().set_param(
-            'auth_saml.allow_saml.uid_and_internal_password',
+        self.env["ir.config_parameter"].sudo().set_param(
+            "auth_saml.allow_saml.uid_and_internal_password",
             self.allow_saml_uid_and_internal_password,
         )
         return super().set_values()
