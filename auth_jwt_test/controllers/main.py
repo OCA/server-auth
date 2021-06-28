ee1a7f099b6a895f582f1a60d8d700162677f206
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-06-28 13:04:08 +0200

    auth_jwt_test: add single page app demo

--- auth_jwt_test/controllers/main.py
+++ auth_jwt_test/controllers/main.py
@@ -21,3 +21,23 @@ class JWTTestController(Controller):
             partner = request.env["res.partner"].browse(request.jwt_partner_id)
             data.update(name=partner.name, email=partner.email)
         return Response(json.dumps(data), content_type="application/json", status=200)
+
+    @route(
+        "/auth_jwt_test/keycloak/whoami",
+        type="http",
+        auth="jwt_demo_keycloak",
+        csrf=False,
+        cors="*",
+        save_session=False,
+    )
+    def whoami_keycloak(self):
+        """To use with the demo_keycloak validator.
+
+        You can play with this using the browser app in tests/spa and the
+        identity provider in tests/keycloak.
+        """
+        data = {}
+        if request.jwt_partner_id:
+            partner = request.env["res.partner"].browse(request.jwt_partner_id)
+            data.update(name=partner.name, email=partner.email)
+        return Response(json.dumps(data), content_type="application/json", status=200)
