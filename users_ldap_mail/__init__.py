745e97eaf20b82bfbd3e5f58404e03aa3815750f
Sergio Zanchetta <primes2h@gmail.com>
2021-07-17 17:31:40 +0200

    [MIG] users_ldap_mail: Migration to 14.0

--- users_ldap_mail/__init__.py
+++ users_ldap_mail/__init__.py
@@ -1,4 +1,4 @@
 # Copyright Daniel Reis (https://launchpad.com/~dreis-pt)
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 from . import models
