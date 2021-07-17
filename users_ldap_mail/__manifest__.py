c28feb97847a59f8b5d7b02f4aa63c271d1112fe
Sergio Zanchetta <primes2h@gmail.com>
2021-07-17 17:31:40 +0200

    [MIG] users_ldap_mail: Migration to 14.0

--- users_ldap_mail/__manifest__.py
+++ users_ldap_mail/__manifest__.py
@@ -1,10 +1,10 @@
 # Copyright Daniel Reis (https://launchpad.com/~dreis-pt)
 # Copyright 2021 Tecnativa - João Marques
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/gpl.html).
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
 
 {
     "name": "LDAP mapping for user name and e-mail",
-    "version": "13.0.1.0.0",
+    "version": "14.0.1.0.0",
     "depends": ["auth_ldap"],
     "author": "Daniel Reis," "Odoo Community Association (OCA)",
     "maintainers": ["joao-p-marques"],
