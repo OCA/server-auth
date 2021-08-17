06577e8097daf79f4365502c9d1e453395c8ff0f
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-08-17 15:10:17 +0200

    [MIG] auth_jwt: _authenticate signature changed

--- auth_jwt/models/ir_http.py
+++ auth_jwt/models/ir_http.py
@@ -24,7 +24,7 @@ class IrHttpJwt(models.AbstractModel):
     _inherit = "ir.http"
 
     @classmethod
-    def _authenticate(cls, endpoint):
+    def _authenticate(cls, auth_method="user"):
         """Protect the _authenticate method.
 
         This is to ensure that the _authenticate method is called
@@ -32,7 +32,6 @@ class IrHttpJwt(models.AbstractModel):
         When migrating, review this method carefully by reading the original
         _authenticate method and make sure the conditions have not changed.
         """
-        auth_method = endpoint.routing["auth"]
         if auth_method == "jwt" or auth_method.startswith("jwt_"):
             if request.session.uid:
                 _logger.warning(
@@ -47,7 +46,7 @@ class IrHttpJwt(models.AbstractModel):
                     'A route with auth="jwt" should not have a request.uid here.'
                 )
                 raise UnauthorizedSessionMismatch()
-        return super(IrHttpJwt, cls)._authenticate(endpoint)
+        return super(IrHttpJwt, cls)._authenticate(auth_method)
 
     @classmethod
     def _auth_method_jwt(cls, validator_name=None):
