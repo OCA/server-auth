326c05ecfda6cc1668ee906f948cd79f8c622154
Simone Orsi <simahawk@gmail.com>
2021-09-24 11:27:14 +0200

    auth_api_key: get rid of server env

--- auth_api_key/tests/test_auth_api_key.py
+++ auth_api_key/tests/test_auth_api_key.py
@@ -3,8 +3,6 @@
 from odoo.exceptions import AccessError, ValidationError
 from odoo.tests.common import SavepointCase
 
-from odoo.addons.server_environment import serv_config
-
 
 class TestAuthApiKey(SavepointCase):
     @classmethod
@@ -15,28 +13,12 @@ class TestAuthApiKey(SavepointCase):
         cls.api_key_good = cls.AuthApiKey.create(
             {"name": "good", "user_id": cls.demo_user.id, "key": "api_key"}
         )
-        cls.api_key_from_env = cls.AuthApiKey.create(
-            {"name": "from_env", "key": "dummy", "user_id": cls.demo_user.id}
-        )
-        cls.api_key_from_env.refresh()
-        serv_config.add_section("api_key_from_env")
-        serv_config.set("api_key_from_env", "key", "api_key_from_env")
 
     def test_lookup_key_from_db(self):
         demo_user = self.env.ref("base.user_demo")
         self.assertEqual(
             self.env["auth.api.key"]._retrieve_uid_from_api_key("api_key"), demo_user.id
         )
-
-    def test_lookup_key_from_env(self):
-        self.assertEqual(
-            self.env["auth.api.key"]._retrieve_uid_from_api_key("api_key_from_env"),
-            self.demo_user.id,
-        )
-        with self.assertRaises(ValidationError):
-            # dummy key must be replace with the one from env and
-            # therefore should be unusable
-            self.env["auth.api.key"]._retrieve_uid_from_api_key("dummy")
 
     def test_wrong_key(self):
         with self.assertRaises(ValidationError), self.env.cr.savepoint():
