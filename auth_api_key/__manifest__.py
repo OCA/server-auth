42347498f9f228a8deea20135917f0b483b2dc1f
Simone Orsi <simahawk@gmail.com>
2021-09-24 11:59:04 +0200

    auth_api_key: 14.0.2.0.0

--- auth_api_key/__manifest__.py
+++ auth_api_key/__manifest__.py
@@ -5,11 +5,10 @@
     "name": "Auth Api Key",
     "summary": """
         Authenticate http requests from an API key""",
-    "version": "14.0.1.0.1",
+    "version": "14.0.2.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-auth",
     "development_status": "Beta",
     "data": ["security/ir.model.access.csv", "views/auth_api_key.xml"],
-    "demo": [],
 }
