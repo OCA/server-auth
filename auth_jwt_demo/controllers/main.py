af7142bc640e0c0a15d918a9b846480d06617fe1
Stéphane Bidoul <stephane.bidoul@acsone.eu>
2021-10-05 09:44:53 +0200

    [IMP] auth_jwt: add public_or_jwt auth method
    This method is useful for public endpoints that need
    to work for anonymous user, but can be enhanced when
    an authenticated user is know.
    
    A typical use case is a "add to cart" enpoint that can
    work for anonymous users, but can be enhanced by
    binding the cart to a known customer when the authenticated
    user is known.

--- auth_jwt_demo/controllers/main.py
+++ auth_jwt_demo/controllers/main.py
@@ -43,3 +43,27 @@ class JWTTestController(Controller):
             partner = request.env["res.partner"].browse(request.jwt_partner_id)
             data.update(name=partner.name, email=partner.email)
         return Response(json.dumps(data), content_type="application/json", status=200)
+
+    @route(
+        "/auth_jwt_demo/keycloak/whoami-public-or-jwt",
+        type="http",
+        auth="public_or_jwt_demo_keycloak",
+        csrf=False,
+        cors="*",
+        save_session=False,
+        methods=["GET", "OPTIONS"],
+    )
+    def whoami_public_or_keycloak(self):
+        """To use with the demo_keycloak validator.
+
+        You can play with this using the browser app in tests/spa and the
+        identity provider in tests/keycloak.
+        """
+        data = {}
+        if hasattr(request, "jwt_partner_id") and request.jwt_partner_id:
+            partner = request.env["res.partner"].browse(request.jwt_partner_id)
+            data.update(name=partner.name, email=partner.email)
+        else:
+            # public
+            data.update(name="Anonymous")
+        return Response(json.dumps(data), content_type="application/json", status=200)
