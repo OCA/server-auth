c074c06be42902c22364bc4ef04a2d7d10213478
Maksym Yankin <yankinmk@gmail.com>
2021-11-26 18:21:08 +0200

    [MIG] auth_api_key_server_env: Migration to 15.0

--- auth_api_key_server_env/tests/test_auth_api_key.py
+++ auth_api_key_server_env/tests/test_auth_api_key.py
@@ -3,12 +3,12 @@
 # @author: Simone Orsi <simone.orsi@camptocamp.com>
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 from odoo.exceptions import ValidationError
-from odoo.tests.common import SavepointCase
+from odoo.tests.common import TransactionCase
 
 from odoo.addons.server_environment import serv_config
 
 
-class TestAuthApiKey(SavepointCase):
+class TestAuthApiKey(TransactionCase):
     @classmethod
     def setUpClass(cls, *args, **kwargs):
         super().setUpClass(*args, **kwargs)
