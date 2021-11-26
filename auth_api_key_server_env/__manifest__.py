317922cabf1ba2f27e956c9305d004281ae71c5d
Maksym Yankin <yankinmk@gmail.com>
2021-11-26 18:21:08 +0200

    [MIG] auth_api_key_server_env: Migration to 15.0

--- auth_api_key_server_env/__manifest__.py
+++ auth_api_key_server_env/__manifest__.py
@@ -11,7 +11,7 @@ This can be very useful to avoid mixing your keys between your various
 environments when restoring databases. All you have to do is to add a new
 section to your configuration file according to the following convention:
     """,
-    "version": "14.0.1.0.1",
+    "version": "15.0.1.0.0",
     "development_status": "Alpha",
     "license": "LGPL-3",
     "website": "https://github.com/OCA/server-auth",
