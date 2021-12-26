5fbb3f7a41762006fa22e10ca01f79e1eafe4bbc
miguelpadin <miguel.padin.dominguez@gmail.com>
2021-12-26 12:07:29 +0100

    [IMP] auth jwt with user credentials

---
+++ auth_jwt_login/models/auth_jwt_validator.py
@@ -0,0 +1,28 @@
+from odoo import _, api, fields, models, tools
+from odoo.exceptions import ValidationError
+
+
+class AuthJwtValidator(models.Model):
+    _inherit = "auth.jwt.validator"
+
+    user_id_strategy = fields.Selection(
+        selection_add=[("login", "Login")],
+        ondelete={'login': 'cascade'},
+    )
+
+    def _get_uid(self, payload):
+        if self.user_id_strategy == 'login':
+            if 'username' in payload and 'password' in payload:
+                user = self.env['res.users'].search(
+                    [
+                        ("login", "=", payload['username'])
+                    ]
+                )
+                if not user:
+                    raise ValidationError
+                user.with_user(user)._check_credentials(payload['password'], None)
+                return user.id
+            else:
+                raise ValidationError
+        else:
+            return super()._get_uid(payload)
