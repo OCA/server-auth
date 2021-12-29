081e2e8753676c0014a313745cf99f9528ff8951
Maksym Yankin <yankinmk@gmail.com>
2021-12-29 10:39:23 +0200

    auth_jwt_demo: Relicence under LGPL

--- auth_jwt_demo/__manifest__.py
+++ auth_jwt_demo/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2021 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Auth JWT Test",
     "summary": """
         Test/demo module for auth_jwt.""",
-    "version": "14.0.1.1.0",
-    "license": "AGPL-3",
+    "version": "14.0.1.2.0",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["sbidoul"],
     "website": "https://github.com/OCA/server-auth",
