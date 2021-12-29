201d1f52476245ba39ad7a304e231c5944448b56
Maksym Yankin <yankinmk@gmail.com>
2021-12-29 10:37:59 +0200

    auth_jwt: Relicence under LGPL

--- auth_jwt/__manifest__.py
+++ auth_jwt/__manifest__.py
@@ -1,12 +1,12 @@
 # Copyright 2021 ACSONE SA/NV
-# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
 
 {
     "name": "Auth JWT",
     "summary": """
         JWT bearer token authentication.""",
     "version": "14.0.1.1.0",
-    "license": "AGPL-3",
+    "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["sbidoul"],
     "website": "https://github.com/OCA/server-auth",
