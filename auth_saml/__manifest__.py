f1c2df14e893e2206e656b82f6091c2c4b258f0f
Vincent Hatakeyama <vincent.hatakeyama@xcg-consulting.fr>
2022-02-14 17:04:52 +0100

    🚑 Fix the module by adding a transaction to commit the token 🚑 Fix the disallow password for users with SAML ids Added tests to ensure the feature works correctly. Admin user is also an exception from not having a password. In Odoo 15.0, this is the standard user to connect for administrative task, not the super user. ✨ Improve provider form and list views ✨⏩ port of 11.0 automatic redirection from 11.0 version. Use disable_autoredirect as a parameter query to disable automatic redirection (for example https://example.com/web/login?disable_autoredirect=) 💄 Add certificate file name fields to improve the UI 📝 Add required on several fields of the SAML provider; without them the server will crash and there is not enough information to make SAML work. ✨ Split signing to have finer control and be compatible with more IDP. 🔨 Integrate token into res.users.saml, removing auth_saml.token. No need for a separate table, and no more need to create lines in the table. 📝 Avoid server errors when user try metadata page without necessary parameters. 🚑 Replace method call from odoo.http.redirect_with_hash to request.redirect as the former does not exists in Odoo 15.0 anymore. 📚 Improved the module documentation 👕 pylint fixes and other fixes or minor changes

--- auth_saml/__manifest__.py
+++ auth_saml/__manifest__.py
@@ -1,9 +1,9 @@
 # Copyright (C) 2020 GlodoUK <https://www.glodo.uk/>
-# Copyright (C) 2010-2016 XCG Consulting <http://odoo.consulting>
+# Copyright (C) 2010-2016, 2022 XCG Consulting <http://odoo.consulting>
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    "name": "Saml2 Authentication",
+    "name": "SAML2 Authentication",
     "version": "15.0.1.0.0",
     "category": "Tools",
     "author": "XCG Consulting, Odoo Community Association (OCA)",
@@ -12,6 +12,8 @@
     "depends": ["base_setup"],
     "external_dependencies": {
         "python": ["pysaml2"],
+        "bin": ["xmlsec1"],
+        # special definition used by OCA to install packages
         "deb": ["xmlsec1"],
     },
     "demo": [],
@@ -24,4 +26,5 @@
     ],
     "installable": True,
     "auto_install": False,
+    "development_status": "Beta",
 }
--- auth_saml/readme/INSTALL.rst
+++ auth_saml/readme/INSTALL.rst
@@ -1,3 +1,3 @@
-This addon requires ``pysaml2``.
+This addon requires the python module ``pysaml2``.
 
-``pysaml2`` requires ``xmlsec1`` (on Ubuntu you can ``apt-get install xmlsec1``)
+``pysaml2`` requires the binary ``xmlsec1`` (on Debian or Ubuntu you can install it with ``apt-get install xmlsec1``)
