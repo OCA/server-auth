b6d1645b294e6ecbb156b393bcac97adc12095e4
BT-vgabor <valentin.gabor@bt-group.com>
2022-02-22 15:34:16 +0100

    Pre-commit fixes

--- password_security/tests/test_totp.py
+++ password_security/tests/test_totp.py
@@ -1,15 +1,17 @@
 # Copyright 2022 Braintec AG
 # License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
 
-from odoo.tests import tagged, HttpCase
 from datetime import datetime, timedelta
-@tagged('post_install', '-at_install')
-class TestTOTP(HttpCase):
 
+from odoo.tests import HttpCase, tagged
+
+
+@tagged("post_install", "-at_install")
+class TestTOTP(HttpCase):
     def test_totp(self):
         # 1. Login with demo user
-        uid = self.env.ref('base.user_demo').id
-        self.assertEqual(uid, self.env.ref('base.user_demo').id)
+        uid = self.env.ref("base.user_demo").id
+        self.assertEqual(uid, self.env.ref("base.user_demo").id)
 
         # 2. Check that we are logged in
         self.authenticate(user="demo", password="demo")
@@ -19,8 +21,10 @@ class TestTOTP(HttpCase):
         self.assertEqual(self.env.user._password_has_expired(), False)
         self.env.user.action_expire_password()
         date_tomorrow = datetime.now() + timedelta(days=1)
-        self.assertEqual(self.env.user.partner_id.signup_expiration.strftime('%m/%d/%Y %H:%M:%S'),
-                         date_tomorrow.strftime('%m/%d/%Y %H:%M:%S'))
+        self.assertEqual(
+            self.env.user.partner_id.signup_expiration.strftime("%m/%d/%Y %H:%M:%S"),
+            date_tomorrow.strftime("%m/%d/%Y %H:%M:%S"),
+        )
 
         self.logout()
         self.assertNotEqual(self.session.uid, uid)
