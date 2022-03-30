1886a851a429c315698d1d25ebe5024c49bd4cef
Eugene Molotov <molotov@it-projects.info>
2022-03-30 17:57:16 +0500

    [MIG] auth_signup_verify_email: Migration to 15.0

--- auth_signup_verify_email/__manifest__.py
+++ auth_signup_verify_email/__manifest__.py
@@ -3,7 +3,7 @@
 {
     "name": "Verify email at signup",
     "summary": "Force uninvited users to use a good email for signup",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Authentication",
     "website": "https://github.com/OCA/server-auth",
     "author": "Antiun Ingeniería S.L., "
