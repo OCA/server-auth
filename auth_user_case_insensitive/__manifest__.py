488c4bb1da699081ab48d779fbd1ba5059f0906e
herqs <herqsss@gmail.com>
2022-04-11 11:47:35 +0300

    [MIG] auth_user_case_insensitive: Migration to 15.0

--- auth_user_case_insensitive/__manifest__.py
+++ auth_user_case_insensitive/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Case Insensitive Logins",
     "summary": "Makes the user login field case insensitive",
-    "version": "14.0.1.0.0",
+    "version": "15.0.1.0.0",
     "category": "Authentication",
     "website": "https://github.com/OCA/server-auth",
     "author": "LasLabs, Odoo Community Association (OCA)",
