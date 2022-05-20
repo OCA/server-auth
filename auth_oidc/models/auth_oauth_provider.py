c7529aad74c2085d70741b01c3e266742e007bf2
Karl Southern <karl@theangryangel.co.uk>
2022-05-20 09:59:33 +0100

    fix(auth_oidc): upstream now starts injecting values that dont exist https://github.com/odoo/odoo/commit/fb3c4845b1549bc2e1378620a5f01e52aa4dbbdb, based on guess work, unfortunately in the case of Azure Active Directory the user_id it copies across is difficult to find to provision users with. since it now exists due to upstream, the token mapping we were using (to copy the UPN) now fails without this patch

--- auth_oidc/models/auth_oauth_provider.py
+++ auth_oidc/models/auth_oauth_provider.py
@@ -60,8 +60,7 @@ class AuthOauthProvider(models.Model):
         if self.token_map:
             for pair in self.token_map.split(" "):
                 from_key, to_key = [k.strip() for k in pair.split(":", 1)]
-                if to_key not in res:
-                    res[to_key] = res.get(from_key, "")
+                res[to_key] = res.get(from_key, "")
         return res
 
     def _parse_id_token(self, id_token, access_token):
