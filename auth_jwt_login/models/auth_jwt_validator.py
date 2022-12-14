3eb9d20a8e94a3d9390ff2cf1915c0d5c470b434
Darío Lodeiros <dario@commitsun.com>
2022-12-14 11:22:14 +0100

    [IMP]auth_kwt_validator: Add interactive loging key

--- auth_jwt_login/models/auth_jwt_validator.py
+++ auth_jwt_login/models/auth_jwt_validator.py
@@ -2,7 +2,7 @@ import time
 
 import werkzeug
 
-from odoo import _, api, fields, models, tools
+from odoo import _, fields, models
 from odoo.exceptions import ValidationError
 
 
@@ -11,25 +11,25 @@ class AuthJwtValidator(models.Model):
 
     user_id_strategy = fields.Selection(
         selection_add=[("login", "Login")],
-        ondelete={'login': 'cascade'},
+        ondelete={"login": "cascade"},
     )
 
     def _get_uid(self, payload):
-        if self.user_id_strategy == 'login':
-            timestamp_expiration_date = payload['exp']
+        if self.user_id_strategy == "login":
+            timestamp_expiration_date = payload["exp"]
             if timestamp_expiration_date:
                 timestamp_now = int(time.time() * 1000.0)
                 if (timestamp_expiration_date - timestamp_now) < 0:
                     raise werkzeug.exceptions.Forbidden(_("Token not valid."))
-            if 'username' in payload and 'password' in payload:
-                user = self.env['res.users'].search(
-                    [
-                        ("login", "=", payload['username'])
-                    ]
+            if "username" in payload and "password" in payload:
+                user = self.env["res.users"].search(
+                    [("login", "=", payload["username"])]
                 )
                 if not user:
                     raise ValidationError
-                user.with_user(user)._check_credentials(payload['password'], None)
+                user.with_user(user)._check_credentials(
+                    payload["password"], {"interactive": True}
+                )
                 return user.id
             else:
                 raise ValidationError
