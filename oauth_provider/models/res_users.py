762177e8590f10f6aab5aa203677e3be42137a59
Rémy Taymans <remytms@tsmail.eu>
2023-01-26 19:49:09 +0100

    [MIG] oauth_provider: black, prettier, isort

--- oauth_provider/models/res_users.py
+++ oauth_provider/models/res_users.py
@@ -2,18 +2,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 import uuid
-from odoo import models, fields
+
+from odoo import fields, models
 
 
 class ResUsers(models.Model):
-    _inherit = 'res.users'
+    _inherit = "res.users"
 
     oauth_identifier = fields.Char(
-        string='OAuth Identifier', required=True, readonly=True,
-        default=lambda self: str(uuid.uuid4()), copy=False,
-        help='String used to identify this user during an OAuth session.')
+        string="OAuth Identifier",
+        required=True,
+        readonly=True,
+        default=lambda self: str(uuid.uuid4()),
+        copy=False,
+        help="String used to identify this user during an OAuth session.",
+    )
 
     _sql_constraints = [
-        ('oauth_identifier_unique', 'UNIQUE (oauth_identifier)',
-         'The OAuth identifier of the user must be unique !'),
+        (
+            "oauth_identifier_unique",
+            "UNIQUE (oauth_identifier)",
+            "The OAuth identifier of the user must be unique !",
+        ),
     ]
