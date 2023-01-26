1c5787397e692112cea6d674b949073c0b5d6c1f
Rémy Taymans <remytms@tsmail.eu>
2023-01-26 19:49:09 +0100

    [MIG] oauth_provider: black, prettier, isort

--- oauth_provider/__init__.py
+++ oauth_provider/__init__.py
@@ -8,16 +8,17 @@ import uuid
 
 
 def pre_init_hook(cr):
-    """ Initialize oauth_identifier on res.users
+    """Initialize oauth_identifier on res.users
 
     The standard initialization puts the same value for every existing record,
     which is invalid for this field.
     This is done in the pre_init_hook to be able to add the unique constrait
     on the first run, when installing the module.
     """
-    cr.execute('ALTER TABLE res_users ADD COLUMN oauth_identifier varchar')
-    cr.execute('SELECT id FROM res_users')
+    cr.execute("ALTER TABLE res_users ADD COLUMN oauth_identifier varchar")
+    cr.execute("SELECT id FROM res_users")
     for user_id in cr.fetchall():
         cr.execute(
-            'UPDATE res_users SET oauth_identifier = %s WHERE id = %s',
-            (str(uuid.uuid4()), user_id))
+            "UPDATE res_users SET oauth_identifier = %s WHERE id = %s",
+            (str(uuid.uuid4()), user_id),
+        )
--- oauth_provider/__manifest__.py
+++ oauth_provider/__manifest__.py
@@ -2,26 +2,26 @@
 # License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 
 {
-    'name': 'OAuth Provider',
-    'summary': 'Allows to use Odoo as an OAuth2 provider',
-    'version': '12.0.1.0.0',
-    'category': 'Authentication',
-    'website': 'https://github.com/oca/server-auth',
-    'author': 'SYLEAM, Odoo Community Association (OCA)',
-    'license': 'AGPL-3',
-    'installable': True,
-    'external_dependencies': {
-        'python': ['oauthlib'],
+    "name": "OAuth Provider",
+    "summary": "Allows to use Odoo as an OAuth2 provider",
+    "version": "12.0.1.0.0",
+    "category": "Authentication",
+    "website": "https://github.com/OCA/server-auth",
+    "author": "SYLEAM, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "installable": True,
+    "external_dependencies": {
+        "python": ["oauthlib"],
     },
-    'depends': [
-        'base',
+    "depends": [
+        "base",
     ],
-    'data': [
-        'security/oauth_provider_security.xml',
-        'security/ir.model.access.csv',
-        'views/oauth_provider_client.xml',
-        'views/oauth_provider_scope.xml',
-        'templates/authorization.xml',
+    "data": [
+        "security/oauth_provider_security.xml",
+        "security/ir.model.access.csv",
+        "views/oauth_provider_client.xml",
+        "views/oauth_provider_scope.xml",
+        "templates/authorization.xml",
     ],
-    'pre_init_hook': 'pre_init_hook',
+    "pre_init_hook": "pre_init_hook",
 }
