d54963f57284fc33d49e1d178efb84a523afedf9
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2023-03-21 15:30:58 +0100

    auth_api_key: archive key when user is archived
    An archived user should not have an active api key anymore.
    But to stay backward compatible the migration script will
    keep all key active.

--- auth_api_key/__manifest__.py
+++ auth_api_key/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Auth Api Key",
     "summary": """
         Authenticate http requests from an API key""",
-    "version": "14.0.2.2.1",
+    "version": "14.0.3.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-auth",
