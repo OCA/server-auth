a70b585e94f0912a9cbe6277eaf2e4e1b50f141e
Szeka Wong <szeka.wong@xcg-consulting.fr>
2023-03-27 09:34:47 +0200

    [MIG] auth_saml: Migration to 16.0

--- auth_saml/models/res_users.py
+++ auth_saml/models/res_users.py
@@ -88,20 +88,24 @@ class ResUser(models.Model):
             return super()._check_credentials(password, env)
 
         except (AccessDenied, passlib.exc.PasswordSizeError):
-            # since normal auth did not succeed we now try to find if the user
-            # has an active token attached to his uid
-            token = (
-                self.env["res.users.saml"]
-                .sudo()
-                .search(
-                    [
-                        ("user_id", "=", self.env.user.id),
-                        ("saml_access_token", "=", password),
-                    ]
-                )
+            passwd_allowed = (
+                env["interactive"] or not self.env.user._rpc_api_keys_only()
             )
-            if token:
-                return
+            if passwd_allowed and self.env.user.active:
+                # since normal auth did not succeed we now try to find if the user
+                # has an active token attached to his uid
+                token = (
+                    self.env["res.users.saml"]
+                    .sudo()
+                    .search(
+                        [
+                            ("user_id", "=", self.env.user.id),
+                            ("saml_access_token", "=", password),
+                        ]
+                    )
+                )
+                if token:
+                    return
             raise AccessDenied() from None
 
     @api.model
@@ -159,7 +163,7 @@ class ResUser(models.Model):
                 "UPDATE res_users SET password = NULL WHERE id IN %s",
                 (tuple(blank_password_users.ids),),
             )
-            self.invalidate_cache(["password"], blank_password_users.ids)
+            blank_password_users.invalidate_recordset(fnames=["password"])
         return
 
     def allow_saml_and_password_changed(self):
