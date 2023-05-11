514ea764e639b8e93c1f797975f367d0921e708d
Yadier A. De Quesada <yadier.dequesada@tecnativa.com>
2023-05-11 10:42:05 -0400

    [MIG] users_ldap_mail: Migration to 16.0

--- users_ldap_mail/__manifest__.py
+++ users_ldap_mail/__manifest__.py
@@ -4,7 +4,7 @@
 
 {
     "name": "LDAP mapping for user name and e-mail",
-    "version": "14.0.1.0.1",
+    "version": "16.0.1.0.0",
     "depends": ["auth_ldap"],
     "author": "Daniel Reis," "Odoo Community Association (OCA)",
     "maintainers": ["joao-p-marques"],
