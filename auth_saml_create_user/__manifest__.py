ec36f68cb76300292e22daeb05429d26af695164
matteoopenf <matteomircoli@openforce.it>
2023-05-31 11:56:50 +0200

    [IMP][auth_saml_create_user] bump version
    [IMP][auth_saml_create_user] fix pre-commit
    
    [IMP][auth_saml_create_user] fix ir access model csv
    
    [IMP][auth_saml_create_user] fix ir access model csv
    
    [IMP][auth_saml_create_user] fix tab spaces

--- auth_saml_create_user/__manifest__.py
+++ auth_saml_create_user/__manifest__.py
@@ -11,11 +11,12 @@
     "website": "https://github.com/OCA/server-auth",
     "license": "AGPL-3",
     "category": "Tools",
-    "version": "11.0.1.0.1",
+    "version": "14.0.1.0.1",
     "depends": ["auth_saml"],
     "data": [
         "data/auth_saml_create_user.xml",
         "views/auth_saml.xml",
+        "security/ir.model.access.csv",
     ],
     "development_status": "Production/Stable",
 }
