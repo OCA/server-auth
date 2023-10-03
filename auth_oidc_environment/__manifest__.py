f915b09468515fa8f0b94322abd6596f090295eb
Adrien Peiffer <adrien.peiffer@acsone.eu>
2023-10-03 17:33:26 +0200

    Add auth_oidc_environment

---
+++ auth_oidc_environment/__manifest__.py
@@ -0,0 +1,18 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Auth Oidc Environment",
+    "summary": """
+        This module allows to use server env for OIDC configuration""",
+    "version": "16.0.1.0.0",
+    "license": "AGPL-3",
+    "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/server-auth",
+    "depends": [
+        "auth_oidc",
+        "server_environment",
+    ],
+    "data": [],
+    "demo": [],
+}
