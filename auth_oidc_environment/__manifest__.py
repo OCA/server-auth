ac74f1ace5344e6622973c3dc2eaf4e46763b26e
Mourad <mourad.elhadj.mimoune@akretion.com>
2023-11-06 14:32:18 +0100

    [14.0][BACKPORT] auth_oidc_environment

--- auth_oidc_environment/__manifest__.py
+++ auth_oidc_environment/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Auth Oidc Environment",
     "summary": """
         This module allows to use server env for OIDC configuration""",
-    "version": "16.0.1.0.0",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-auth",
