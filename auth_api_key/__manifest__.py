19b09b4bbc61fe8c802e33b72ea1a2c8775456c5
Chris Bergman <33776709+chrisb-c01@users.noreply.github.com>
2023-12-16 15:17:34 +0100

    [MIG] auth_api_key: Migration to 17.0

--- auth_api_key/__manifest__.py
+++ auth_api_key/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Auth Api Key",
     "summary": """
         Authenticate http requests from an API key""",
-    "version": "16.0.1.0.0",
+    "version": "17.0.1.0.0",
     "license": "LGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-auth",
