7e0d479c953989a0f5584f3d9be23eef202d2b52
mle <marie.lejeune@acsone.eu>
2024-01-09 14:26:41 +0100

    [MIG] auth_oauth_autologin: Migration to 14.0

--- auth_oauth_autologin/__manifest__.py
+++ auth_oauth_autologin/__manifest__.py
@@ -5,7 +5,7 @@
     "name": "Auth Oauth Autologin",
     "summary": """
         Automatically redirect to the OAuth provider for login""",
-    "version": "13.0.1.0.2",
+    "version": "14.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV,Odoo Community Association (OCA)",
     "maintainers": ["sbidoul"],
