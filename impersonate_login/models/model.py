84a704f04326d547bbdfedf725ff6bda06d39014
Kev-Roche <kevin.roche@akretion.com>
2024-05-14 09:54:25 +0200

    [14.0][ADD] impersonate_login

---
+++ impersonate_login/models/model.py
@@ -0,0 +1,24 @@
+# Copyright (C) 2024 Akretion (<http://www.akretion.com>).
+# @author Kévin Roche <kevin.roche@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo import api, models
+from odoo.http import request
+
+
+class BaseModel(models.AbstractModel):
+    _inherit = "base"
+
+    @api.model_create_multi
+    def _create(self, data_list):
+        res = super()._create(data_list)
+        if request and request.session.impersonate_from_uid:
+            for rec in res:
+                rec.create_uid = request.session.impersonate_from_uid
+        return res
+
+    def write(self, vals):
+        res = super().write(vals)
+        if request and request.session.impersonate_from_uid:
+            self.write_uid = request.session.impersonate_from_uid
+        return res
