9b91db7e8bf786c1197ef79a6874b49ba79d416e
Thierry Ducrest <thierry.ducrest@camptocamp.com>
2025-06-24 11:58:05 +0200

    [MIG] auth_jwt_server_env: Migration to 18.0

--- auth_jwt_server_env/__manifest__.py
+++ auth_jwt_server_env/__manifest__.py
@@ -4,7 +4,7 @@
 {
     "name": "Auth Jwt Server Env",
     "summary": """This addon adds auth.jwt.validator fields to server env""",
-    "version": "16.0.1.0.0",
+    "version": "18.0.1.0.0",
     "license": "AGPL-3",
     "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
     "website": "https://github.com/OCA/server-auth",
