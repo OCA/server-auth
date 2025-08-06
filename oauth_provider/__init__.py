6917bb3ee26be923562d80112229d011b36c120c
augusto-weiss <awe@adhoc.com.ar>
2025-08-06 11:48:03 +0000

    [MIG] oauth_provider: Migration to 18.0
    user data

--- oauth_provider/__init__.py
+++ oauth_provider/__init__.py
@@ -7,7 +7,7 @@ from . import models
 import uuid
 
 
-def pre_init_hook(cr):
+def pre_init_hook(env):
     """Initialize oauth_identifier on res.users
 
     The standard initialization puts the same value for every existing record,
@@ -15,10 +15,10 @@ def pre_init_hook(cr):
     This is done in the pre_init_hook to be able to add the unique constrait
     on the first run, when installing the module.
     """
-    cr.execute("ALTER TABLE res_users ADD COLUMN oauth_identifier varchar")
-    cr.execute("SELECT id FROM res_users")
-    for user_id in cr.fetchall():
-        cr.execute(
+    env.cr.execute("ALTER TABLE res_users ADD COLUMN oauth_identifier varchar")
+    env.cr.execute("SELECT id FROM res_users")
+    for user_id in env.cr.fetchall():
+        env.cr.execute(
             "UPDATE res_users SET oauth_identifier = %s WHERE id = %s",
             (str(uuid.uuid4()), user_id),
         )
