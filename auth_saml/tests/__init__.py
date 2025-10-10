b2bd6ddf59d2ba9973cac911bd3f1e4a5a3a751f
len <len@lambdao.dev>
2025-10-10 15:15:40 +0200

    [FIX] auth_saml: do not crash if no users

--- auth_saml/tests/__init__.py
+++ auth_saml/tests/__init__.py
@@ -1,1 +1,4 @@
-from . import fake_idp, test_pysaml, test_models_saml_attribute_mapping
+from . import fake_idp
+from . import test_pysaml
+from . import test_models_saml_attribute_mapping
+from . import test_param_change
