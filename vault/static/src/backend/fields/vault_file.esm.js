3987266eb70304343bf737293c78a7e43abd7a94
Carlos Roca <carlos.roca@tecnativa.com>
2026-02-16 11:32:48 +0100

    [FIX] vault: Error when try to download file
    Right now the data of a bynary field is not saving the real value but is saving the size of the file. So we were trying to decrypt a string like "300 kB".
    
    With this changes, we get the crypted value from the db so it will be decrypted correctly.

--- vault/static/src/backend/fields/vault_file.esm.js
+++ vault/static/src/backend/fields/vault_file.esm.js
@@ -12,13 +12,29 @@ export class VaultFile extends VaultMixin(BinaryField) {
     static template = "vault.FileVault";
     setup() {
         super.setup();
-
+        this.orm = useService("orm");
+        this.notification = useService("notification");
         this.action = useService("action");
     }
 
     async update({data, name}) {
         const encrypted = await this._encrypt(data);
         return await super.update({data: encrypted, name: name});
+    }
+
+    /**
+     * Get the value saved on db because the system is showing just the
+     * size on the props.
+     *
+     * @param {Object} ev
+     */
+    async _getCryptedValue() {
+        const crypted = await this.orm.read(
+            this.props.record.resModel,
+            [this.props.record.resId],
+            [this.props.name]
+        );
+        return crypted[0][this.props.name];
     }
 
     /**
@@ -29,27 +45,28 @@ export class VaultFile extends VaultMixin(BinaryField) {
     async _onSendValue(ev) {
         ev.stopPropagation();
 
-        await this.sendValue("", this.props.value, this.state.fileName);
+        await this.sendValue("", await this._getCryptedValue(), this.fileName);
     }
 
     /**
      * Decrypt the file and download it
      */
     async onFileDownload() {
-        if (!this.props.value) {
-            this.do_warn(
-                _t("Save As..."),
-                _t("The field is empty, there's nothing to save!")
-            );
+        const cryptedValue = await this._getCryptedValue();
+        if (!cryptedValue) {
+            this.notification.add(_t("The field is empty, there's nothing to save!"), {
+                title: _t("Save As..."),
+                type: "danger",
+            });
         } else if (this.vault_utils.supported()) {
-            const decrypted = await this._decrypt(this.props.value);
+            const decrypted = await this._decrypt(cryptedValue);
             const base64 = atob(decrypted);
             const buffer = new ArrayBuffer(base64.length);
             const arr = new Uint8Array(buffer);
             for (let i = 0; i < base64.length; i++) arr[i] = base64.charCodeAt(i);
 
             const blob = new Blob([arr]);
-            await downloadFile(blob, this.state.fileName || "");
+            await downloadFile(blob, this.fileName || "");
         }
     }
 }
