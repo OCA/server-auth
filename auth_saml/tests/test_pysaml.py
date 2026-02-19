c42d7fba49d67b64e6901342e422475a97359c88
Vincent Hatakeyama <vincent.hatakeyama@orbeet.io>
2026-02-19 16:46:30 +0100

    [IMP] auth_saml: add option to display a specific error page when login fails

--- auth_saml/tests/test_pysaml.py
+++ auth_saml/tests/test_pysaml.py
@@ -8,9 +8,11 @@ import urllib
 from copy import deepcopy
 from unittest import SkipTest
 from unittest.mock import patch
+from urllib.parse import urlparse
 
 import responses
 from saml2.sigver import SignatureError
+from saml2.validate import ResponseLifetimeExceed
 
 from odoo.exceptions import AccessDenied, UserError, ValidationError
 from odoo.orm.commands import Command
@@ -531,6 +533,77 @@ class TestPySaml(HttpCase):
             self.base_url()
             + "/web#action=37&model=ir.module.module&view_type=kanban&menu_id=5",
         )
+
+    def test_auth_errors(self):
+        self.add_provider_to_user()
+
+        auth_request = self.saml_provider._get_auth_request()
+        response = self.idp.fake_login(auth_request)
+        unpacked_response = response._unpack()
+
+        for key in unpacked_response:
+            unpacked_response[key] = html.unescape(unpacked_response[key])
+        self.user.active = False
+        with mute_logger("odoo.addons.auth_saml.controllers"):
+            response = self.url_open(
+                "/auth_saml/signin",
+                data=unpacked_response,
+                allow_redirects=True,
+            )
+        self.assertEqual(
+            response.url, f"{self.base_url()}/web/login?saml_error=expired"
+        )
+        self.user.active = True
+        # invalidate saml response
+        unpacked_response["SAMLResponse"] = ""
+        with mute_logger("odoo.addons.auth_saml.controllers"):
+            response = self.url_open(
+                "/auth_saml/signin",
+                data=unpacked_response,
+                allow_redirects=True,
+            )
+        self.assertEqual(
+            response.url, f"{self.base_url()}/web/login?saml_error=access-denied"
+        )
+        self.browse_ref(
+            "auth_saml.saml_error_page_config_parameter"
+        ).value = "/web/login/saml_error"
+        with mute_logger("odoo.addons.auth_saml.controllers"):
+            response = self.url_open(
+                "/auth_saml/signin",
+                data=unpacked_response,
+                allow_redirects=True,
+            )
+        self.assertEqual(
+            response.url,
+            f"{self.base_url()}/web/login/saml_error?saml_error=access-denied",
+        )
+
+        # Not an error easy to reproduce so use a patch to raise it.
+        def auth_saml(*args, **kwargs):
+            raise ResponseLifetimeExceed()
+
+        with (
+            mute_logger("odoo.addons.auth_saml.controllers"),
+            patch(
+                "odoo.addons.auth_saml.models.res_users.ResUser.auth_saml", auth_saml
+            ),
+        ):
+            response = self.url_open(
+                "/auth_saml/signin",
+                data=unpacked_response,
+                allow_redirects=True,
+            )
+        self.assertEqual(
+            response.url,
+            f"{self.base_url()}/web/login/saml_error?saml_error=response-lifetime-exceed",
+        )
+
+    def test_saml_error_page_redirect(self):
+        """Test that accessing the page without an error redirects to root page."""
+        response = self.url_open("/web/login/saml_error")
+        path = urlparse(response.url).path
+        self.assertEqual(path, "/web/login")
 
     def test_disallow_user_password_when_changing_settings(self):
         """Test that disabling the setting will remove passwords from related users"""
