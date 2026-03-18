c5545fea6d122409b7c2fde3c4fed13ddcd0bc45
Kev-Roche <kevin.roche@akretion.com>
2026-03-18 11:32:40 +0100

    [18.0][FIX] impersonate_login access issues

--- impersonate_login/tests/test_impersonate_login.py
+++ impersonate_login/tests/test_impersonate_login.py
@@ -263,3 +263,42 @@ class TestImpersonateLogin(HttpCase):
         self.assertEqual(contact.id, contact_id)
         self.assertEqual(contact.ref, "abc")
         self.assertEqual(contact.write_uid, self.admin_user)
+
+    def test_05_create_uid_on_transient_model(self):
+        """Check the create_uid of records created
+        during an impersonated session on a transient model"""
+        # Login as admin
+        self.authenticate(user="admin", password="admin")
+
+        # Impersonate demo user and create a wizard record
+        self._impersonate_user(self.demo_user)
+
+        response = self.url_open(
+            "/web/dataset/call_kw/mail.wizard.invite/web_save",
+            data=json.dumps(
+                {
+                    "params": {
+                        "model": "mail.wizard.invite",
+                        "method": "web_save",
+                        "args": [
+                            [],
+                            {
+                                "res_model": "res.partner",
+                                "message": "Hello",
+                            },
+                            {},
+                        ],
+                        "kwargs": {},
+                    },
+                }
+            ),
+            headers={"Content-Type": "application/json"},
+        )
+        self.assertEqual(response.status_code, 200)
+        data = response.json()
+        result = data["result"]
+        settings_id = result[0]["id"]
+
+        wizard = self.env["mail.wizard.invite"].browse(settings_id)
+        self.assertIn("Hello", wizard.message)
+        self.assertEqual(wizard.create_uid, self.demo_user)
