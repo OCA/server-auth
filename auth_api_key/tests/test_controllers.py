9ed52968c1f50115b076178b624c78ab5ff325e1
Iván Todorovich <ivan.todorovich@gmail.com>
2026-05-20 16:54:27 -0300

    [FIX] auth_api_key: report allowed header in cors OPTIONS

--- auth_api_key/tests/test_controllers.py
+++ auth_api_key/tests/test_controllers.py
@@ -31,6 +31,10 @@ class TestControllers(HttpCase):
             def auth_api_key(self, **params):
                 return json.dumps({"name": self.env.user.name})
 
+            @route("/web/auth-api-key-cors", type="http", auth="api_key", cors="*")
+            def auth_api_key_cors(self, **params):
+                return json.dumps({"name": self.env.user.name})
+
         cls.env.registry.clear_cache("routing")
         cls.addClassCleanup(cls.env.registry.clear_cache, "routing")
 
@@ -45,3 +49,9 @@ class TestControllers(HttpCase):
             res = self.url_open("/web/auth-api-key", headers={"API-KEY": "wrong"})
         self.assertEqual(res.status_code, 403)
         self.assertIn("Access Denied", cm.output[0])
+
+    def test_auth_api_key_cors_options(self):
+        res = self.url_open("/web/auth-api-key-cors", method="OPTIONS")
+        self.assertEqual(res.status_code, 204)
+        self.assertEqual(res.headers["Access-Control-Allow-Origin"], "*")
+        self.assertIn("API-Key", res.headers["Access-Control-Allow-Headers"])
