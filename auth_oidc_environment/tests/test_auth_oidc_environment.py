33ff042b942a1f334ab575649ae49709f6e20c89
Lindsay <lindsay.marion@acsone.eu>
2026-07-03 13:51:32 +0200

    [19.0][MIG] auth_oidc_environment: Migration to 19.0

--- auth_oidc_environment/tests/test_auth_oidc_environment.py
+++ auth_oidc_environment/tests/test_auth_oidc_environment.py
@@ -12,13 +12,12 @@ from odoo.addons.server_environment.tests.common import ServerEnvironmentCase
 @patch.dict(odoo_config.options, {"running_env": "testing"})
 class TestEnvironmentVariables(ServerEnvironmentCase):
     def test_env_variables(self):
-        env_var = (
-            "[auth_oauth_provider.sample]\n" "client_id=foo\n" "client_secret=bar\n"
-        )
+        env_var = "[auth_oauth_provider.sample]\nclient_id=foo\nclient_secret=bar\n"
         with self.set_config_dir(None), self.set_env_variables(env_var):
             parser = server_env._load_config()
             self.assertEqual(
-                list(parser.keys()), ["DEFAULT", "auth_oauth_provider.sample"]
+                list(parser.keys()),
+                ["DEFAULT", "auth_oauth_provider.sample"],
             )
             self.assertDictEqual(
                 dict(parser["auth_oauth_provider.sample"].items()),
